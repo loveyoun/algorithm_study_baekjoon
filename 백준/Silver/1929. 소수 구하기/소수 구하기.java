0188a8644c46c3e4ac8a0a17be03affbdaaa7143
loveyoun7 <lovekite0406@naver.com>
2023-03-01 11:59:31 +0900

    [Silver III] Title: 소수 구하기, Time: 896 ms, Memory: 37152 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main{
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int M = sc.nextInt();
@@ -8,13 +9,14 @@ public class Main{
         int[] A = new int[N+1];
         for(int i=2;i<=N;i++) A[i] = i;
         
-        for(int i=2;i<=Math.sqrt(N);i++){
+        for(int i=2;i<=Math.sqrt(N);i++){ //에라토스테네스의 체 최적화
             if(A[i] == 0) continue;
-            for(int j=i+i;j<=N;j+=i) A[j] = 0;
+            for(int j=i+i;j<=N;j+=i) A[j] = 0; //소수 아닌 거(배수들) 지우기
         }
         
         for(int i=M;i<=N;i++){
             if(A[i] != 0) System.out.println(A[i]);
         }
     }
+    
 }
