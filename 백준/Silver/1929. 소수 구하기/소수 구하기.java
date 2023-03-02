9ec6bed2e9a56c044fb87b552c5f70bcb88f6b1c
loveyoun7 <lovekite0406@naver.com>
2023-03-02 10:44:41 +0900

    [Silver III] Title: 소수 구하기, Time: 328 ms, Memory: 27132 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main{
     
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-        int[] A = new int[N+1];
-        for(int i=2;i<=N;i++) A[i] = i;
-        
-        for(int i=2;i<=Math.sqrt(N);i++){ //에라토스테네스의 체 최적화
-            if(A[i] == 0) continue;
-            for(int j=i+i;j<=N;j+=i) A[j] = 0; //소수 아닌 거(배수들) 지우기
-        }
-        
-        for(int i=M;i<=N;i++){
-            if(A[i] != 0) System.out.println(A[i]);
-        }
+        try{
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
+
+            String nm[] = input.readLine().split(" ");       
+            int M = Integer.parseInt(nm[0]);
+            int N = Integer.parseInt(nm[1]);
+            int[] A = new int[N+1];
+            for(int i=2;i<=N;i++) A[i] = i;
+
+            for(int i=2;i<=N;i++){
+                if(A[i] == 0) continue; //배수이면, 
+                else{ //소수이면,
+                    if(i >= M) sb.append(i+"\n");
+                    for(int j=i+i;j<=N;j+=i) A[j] = 0; //소수 아닌 거(소수들의 배수) 지우기
+                }
+            }
+
+            System.out.println(sb);
+        }catch(IOException e){return;}
     }
     
 }
