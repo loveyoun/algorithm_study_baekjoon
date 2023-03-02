7f73ed6245cad34ef2bfd9e0699b71de08541f48
loveyoun7 <lovekite0406@naver.com>
2023-03-02 10:48:24 +0900

    [Silver III] Title: 소수 구하기, Time: 312 ms, Memory: 25220 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -10,14 +10,15 @@ public class Main{
             String nm[] = input.readLine().split(" ");       
             int M = Integer.parseInt(nm[0]);
             int N = Integer.parseInt(nm[1]);
-            int[] A = new int[N+1];
-            for(int i=2;i<=N;i++) A[i] = i;
-
+            /*int[] A = new int[N+1];
+            for(int i=2;i<=N;i++) A[i] = i;*/
+            boolean[] nonprime = new boolean[N+1]; //false로 초기화
+            
             for(int i=2;i<=N;i++){
-                if(A[i] == 0) continue; //배수이면, 
+                if(nonprime[i]) continue; //배수이면, 
                 else{ //소수이면,
                     if(i >= M) sb.append(i+"\n");
-                    for(int j=i+i;j<=N;j+=i) A[j] = 0; //소수 아닌 거(소수들의 배수) 지우기
+                    for(int j=i+i;j<=N;j+=i) nonprime[j] = true; //소수 아닌 거(소수들의 배수) 지우기
                 }
             }
 
