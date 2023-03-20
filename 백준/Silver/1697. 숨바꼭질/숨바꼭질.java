8d6ea805ec263cba2babb6738bbeb9c5a13e2357
loveyoun7 <lovekite0406@naver.com>
2023-03-21 06:41:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 19328 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Main {
+
+    static int N, K;
+    static boolean found;
+    static int secs;
+    static int[] dx = {-1, 1};
+    static int new_x;
+
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        /** 1697_Algorithm flow:
+         **/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] stmp = br.readLine().split(" ");
+        N = Integer.parseInt(stmp[0]);
+        K = Integer.parseInt(stmp[1]);
+
+        if (N == K) System.out.println(0);
+        else {
+            visited = new boolean[100001];
+            secs = 0;
+            found = false;
+            BFS(N, 0);
+
+            if (found) System.out.println(secs);
+        }
+    }
+
+    static void BFS(int v, int count) {
+        Queue<Node> queue = new LinkedList<>();
+
+        visited[v] = true;
+        queue.add(new Node(v, count));
+
+        while (queue.size() > 0) {
+            Node node = queue.poll();
+            //== 인접 노드 탐색
+            for (int i = 2; i >= 0; i--) {
+                if (i == 2) new_x = node.x * 2;
+                else new_x = node.x + dx[i];
+
+                if (new_x >= 0 && new_x <= 100000) {
+                    if (!visited[new_x]) {
+                        if (new_x == K) {
+                            found = true;
+                            secs = node.count + 1;
+                            break;
+                        }
+
+                        visited[new_x] = true;
+                        queue.add(new Node(new_x, node.count + 1)); /* ++n.count n.count 도 올라감*/
+                    }
+                } else continue;
+            }
+
+            if (found) break;
+        }
+    }
+
+    static class Node {
+        int x;
+        int count;
+
+        public Node(int x, int count) {
+            this.x = x;
+            this.count = count;
+        }
+    }
+
+}
