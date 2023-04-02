b4f8291c9d04e1ee9b1ed315f7d40edc0a494779
loveyoun7 <lovekite0406@naver.com>
2023-04-03 02:24:49 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45560 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,18 +1,26 @@
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        /** 2164_Algorithm_flow_카드2: 큐(의 정석)
+         **/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Queue<Integer> myQueue = new LinkedList<>();
-        int N = sc.nextInt();
-        for(int i=1;i<=N;i++) myQueue.add(i);
-        
-        while(myQueue.size() > 1){
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) myQueue.add(i);
+
+        while (myQueue.size() > 1) {
             myQueue.poll();
             myQueue.add(myQueue.poll());
         }
-        
+
         System.out.println(myQueue.poll());
     }
+
 }
