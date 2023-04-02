1466c27e465c7fc9d27edafceb6c085ac0be3f9f
loveyoun7 <lovekite0406@naver.com>
2023-04-02 23:39:51 +0900

    [Silver II] Title: 스택 수열, Time: 348 ms, Memory: 27744 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        StringBuffer bf = new StringBuffer();
-        int N = sc.nextInt();
-        int[] A = new int[N];
-        for(int i=0;i<N;i++) A[i] = sc.nextInt();
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        /**1874_Algorithm_flow_스택 수열: 스택 구현
+         **/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine()); //sc.nextInt();
         Stack<Integer> stack = new Stack<>();
         int num = 1;
         boolean result = true;
-        
-        //기작
-        //스택에는 num = 1부터 push 후 증가
-        //A[i] 4 == num4까지 push 후 num++(중복되면 안되니까) 후 pop 후 ---1
-        //A[i] 6 > num5이면 ---2==1
-        //A[i] 3 < num5이면 pop() == A[i]이면 ---3
-        //NO일 때 : '3'에서 pop()이 더 크면 나오면 수열 값이 아니므로
-        for(int i=0;i<A.length;i++){
-            int su = A[i];
-            if(su >= num){
-                while(su >= num){
+
+        /* 기작
+         * 스택에는 num = 1부터 push -> 증가
+         * arr[i] 4 == num 4까지 push -> num++(중복되면 안되니까) -> pop
+         * arr[i] 6 > num 5이면 push (...)
+         * arr[i] 3 < num 5이면 pop() 5 == arr[i] 3이면
+         * 1 2 3 4(x) 5 6(x)
+         * NO일 때 : 위에서 pop()이 더 크거나 작으면, 나오면 수열 값이 아니므로
+         * 작은 수가 나올 수는 없다. 이미 그럼 arr[i]가 나갔다는 뜻이니까.
+         * 결론: 높은 수 -> 낮은 수로 갈 때, 그 중간 값이 있어서는 안 된다.
+         */
+        for (int i = 0; i < arr.length; i++) {
+            int object = arr[i];
+            if (object >= num) { //채워주기만 하면 됨.
+                while (object >= num) {
                     stack.push(num++);
-                    bf.append("+\n");
+                    sb.append("+\n");
                 }
                 stack.pop();
-                bf.append("-\n");
-            }else{
-                int n = stack.pop();
-                if(n != su){
+                sb.append("-\n");
+            } else { //뺄 수 있는지 크기 비교 해주어야 함.
+                int top = stack.pop();
+
+                /* top > object 되긴 함 */
+                if (top != object) {
                     System.out.println("NO");
                     result = false;
                     break;
-                }else bf.append("-\n");
+                } else sb.append("-\n");
             }
         }
-        
-        if(result) System.out.println(bf.toString());
+
+        if (result) System.out.println(sb.toString());
     }
+
 }
