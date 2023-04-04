fbbdfa744d621128b1ae81ec8f54c1f9410bdef8
loveyoun7 <lovekite0406@naver.com>
2023-04-04 13:19:00 +0900

    [level 2] Title: H-Index, Time: 0.46 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -3,18 +3,20 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        
+
         Arrays.sort(citations);
-        
-        for(int h=0;h<=Math.max(citations.length, citations[citations.length-1]);h++){
-            int count = 0;
-            
-            for(int j=0;j<citations.length;j++)
-                if(citations[j] >= h) count++;
-            
-            if(count >= h) answer = Math.max(answer, h);
+
+        int arr_length = citations.length;
+        for (int i = 0; i < arr_length; i++) {
+            int h = arr_length - i; //i = 0 ~ 부터라서, 논문 개수
+
+            if(citations[i] >= h){
+                answer = h;
+                //h가 내림차순으로 내려가니까, break 가능.
+                break;
+            }
         }
-        
+
         return answer;
     }
 }
