ad79001016de28c9a1d6b40e4d5c900de7381ae8
loveyoun7 <lovekite0406@naver.com>
2023-04-16 13:45:57 +0900

    [Silver III] Title: 소수 구하기, Time: 304 ms, Memory: 24336 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,33 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    
-    public static void main(String[] args){
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringBuilder sb = new StringBuilder();
+public class Main {
 
-            /*String nm[] = br.readLine().split(" ");       
-            int M = Integer.parseInt(nm[0]);
-            int N = Integer.parseInt(nm[1]);*/
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int M = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-            /*int[] A = new int[N+1];
-            for(int i=2;i<=N;i++) A[i] = i;*/
-            boolean[] nonprime = new boolean[N+1]; //false로 초기화
-            
-            for(int i=2;i<=N;i++){
-                if(nonprime[i]) continue; //배수이면, 
-                else{ //소수이면,
-                    if(i >= M) sb.append(i+"\n");
-                    for(int j=i+i;j<=N;j+=i) nonprime[j] = true; //소수 아닌 거(소수들의 배수) 지우기
-                }
-            }
+    public static void main(String[] args) throws IOException {
+        /**1929_Algorithm_flow_소수 구하기: 정수론(에라토스테네스의 체)
+         **/
+        
+        /*Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();*/
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-            System.out.println(sb);
-        }catch(IOException e){return;}
+        /*String nm[] = br.readLine().split(" ");       
+        int M = Integer.parseInt(nm[0]);
+        int N = Integer.parseInt(nm[1]);*/
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        /*int[] A = new int[N+1];
+        for(int i=2;i<=N;i++) A[i] = i;*/
+        boolean[] nonprime = new boolean[N + 1]; // 초기값: false
+
+        for (int i = 2; i <= N; i++) {
+            /** 에라토스테네스의 체 최적화 
+             i <= Math.sqrt(N)
+             **/
+            // 배수이면, 
+            if (nonprime[i]) continue;
+            // 소수이면,
+            if (i >= M) sb.append(i + "\n"); // 최적화하면 이 코드 못 씀
+            for (int j = i + i; j <= N; j += i) nonprime[j] = true; // 소수 아닌 거(소수들의 배수) 지우기
+        }
+        
+        System.out.println(sb);
+        /**출력초과**/ //for(int i=2;i<=N;i++) if(!nonprime[i]) System.out.print(i+"\n");
     }
-    
+
 }
