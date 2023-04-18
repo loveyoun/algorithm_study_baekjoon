09c6aaa7ee4cfd0e5a98eaf2eaea0b6c6d4148c3
loveyoun7 <lovekite0406@naver.com>
2023-04-18 21:42:15 +0900

    [Gold IV] Title: 최단경로, Time: 804 ms, Memory: 109664 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -6,76 +6,84 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int V, E, K;
+
+    static int nodes, edges, start;
     static int[] distance;
     static boolean[] visited;
     static ArrayList<Edge>[] graph;
     static PriorityQueue<Edge> pqueue = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
-        /** 1753_Algorithm flow: 다익스트라 (사실 BFS 응용)
-         * **/
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken()); //노드개수
-        E = Integer.parseInt(st.nextToken()); //엣지개수
-        K = Integer.parseInt(br.readLine()); //출발노드번호
-        distance = new int[V + 1];
-        visited = new boolean[V + 1];
-        graph = new ArrayList[V + 1];
-        for (int i = 1; i < V + 1; i++) graph[i] = new ArrayList<>();
-        for (int i = 1; i < V + 1; i++) distance[i] = Integer.MAX_VALUE;
-        for (int i = 0; i < E; i++) {
+        nodes = Integer.parseInt(st.nextToken());
+        edges = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(br.readLine());
+
+        visited = new boolean[nodes + 1];
+        distance = new int[nodes + 1];
+        for (int i = 1; i <= nodes; i++) distance[i] = Integer.MAX_VALUE;
+
+        graph = new ArrayList[nodes + 1];
+        for (int i = 1; i <= nodes; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < edges; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
+
+            // 일방향
             graph[s].add(new Edge(e, v));
         }
 
-        //출발 노드
-        pqueue.add(new Edge(K, 0));
-        distance[K] = 0;
+
+        dijkstra(start);
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= nodes; i++) {
+            if (visited[i]) sb.append(distance[i] + "\n");
+            else sb.append("INF\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void dijkstra(int start) {
+        pqueue.add(new Edge(start, 0));
+        distance[start] = 0;
 
         while (!pqueue.isEmpty()) {
-            Edge current = pqueue.poll();
-            int c_v = current.vertex;
-            if (visited[c_v]) continue;
+            Edge now = pqueue.poll();
+            int now_node = now.node;
+            if (!visited[now_node]) {
+                visited[now_node] = true;
 
-            visited[c_v] = true;
-            for (int i = 0; i < graph[c_v].size(); i++) {
-                Edge tmp = graph[c_v].get(i);
-                int next = tmp.vertex;
-                int value = tmp.value;
-                //전에 값 + 엣지 값 이 작으면
-                if (distance[next] > distance[c_v] + value) {
-                    distance[next] = distance[c_v] + value;
-                    pqueue.add(new Edge(next, distance[next]));
+                for (Edge e : graph[now_node]) {
+                    int next_node = e.node;
+                    int btw_value = e.value;
+                    // 다른 곳에서 온 거리 vs 지금 곳에서 온 거리
+                    if (distance[next_node] > distance[now_node] + btw_value) {
+                        distance[next_node] = distance[now_node] + btw_value;
+                        pqueue.add(new Edge(next_node, distance[next_node]));
+                    }
                 }
             }
         }
-
-        for (int i = 1; i < V + 1; i++) {
-            if (visited[i]) System.out.println(distance[i]);
-
-            //경로 없을 때
-            else System.out.println("INF");
-        }
     }
 
-
     static class Edge implements Comparable<Edge> {
-        int vertex, value;
+        int node, value;
 
-        Edge(int vertex, int value) {
-            this.vertex = vertex;
+        Edge(int node, int value) {
+            this.node = node;
             this.value = value;
         }
 
+        @Override
         public int compareTo(Edge e) {
-            if (this.value > e.value) return 1; // 오름차순
-            else return -1;
+            return this.value - e.value;
         }
     }
 
