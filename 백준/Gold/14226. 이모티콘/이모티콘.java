e57b9cb56c0a4e686e521d5f1ca55e377571b055
loveyoun7 <lovekite0406@naver.com>
2023-05-03 19:53:21 +0900

    [Gold IV] Title: 이모티콘, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -1,111 +1,53 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Main {
-
-    static int N, result;
-    static boolean found;
-    //static int[] monitor;
-    //static boolean[] visited;
-    static boolean[] clip_board;
-
-    public static void main(String[] args) throws IOException {
-        /** 14226_Algorithm_이모티콘 : 다익스트라 (no pure BFS)
-         **/
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int S = Integer.parseInt(br.readLine());
 
-        clip_board = new boolean[2001];
-        found = false;
-        result = 0;
-        Di_BFS(1, 0, 0);
+        final int MAX = 10000;
+        int[] dp = new int[MAX + 1];
 
-        System.out.println(result);
-    }
-
-    static void Di_BFS(int start_len, int copied, int sec) {
-        /*PriorityQueue<Clip> queue = new PriorityQueue<>(((o1, o2) -> {
-            /* 4, 6 처리 _ 숨바꼭질3 
-            4 (3,1) 6 > 4 (5,1) 6
-             
-            if (o1.sec == o2.sec) return o1.len - o2.len;
-            else return o1.sec - o2.sec; // 오름차순
-        }));*/
-        Queue<Clip> queue = new LinkedList<>();
-
-        queue.add(new Clip(start_len, copied, sec));
-
-        while (queue.size() > 0) {
-            Clip now = queue.poll();
-            int now_len = now.len;
-            int now_copied = now.copied;
-            int now_sec = now.sec;
-
-            for (int i = 0; i < 3; i++) {
-                if (i == 0) {
-                    // 1) 복사하기
-                    // (1, 1, 1) -> (1, 1, 2) 되지 않게 clip_board[1] 조건
-                    if (!clip_board[now_len] && now_len > 0) {
-                        queue.add(new Clip(now_len, now_len, now_sec + 1));
-                        /* 우선순위 큐로 해결 됨.
-                        clip_board[now_len] = Math.min(clip_board[now_len], monitor[now_len] + 1);
-                        */
-                        clip_board[now_len] = true;
-                    }
-                } else if (i == 1) {
-                    /** 2) 붙여넣기
-                     1 2 4 (2) 6 12 (6) 18 -> 8
-                     6을 복사했는데, 다음에 2를 붙여넣을 수 없지 않냐? 를 생각할 의미는 없다.
-                     1)에서 새로운 복붙을 갱신해주기 때문에. 유남생?
-                     붙여넣기는 붙여넣기만 해주고, 복사는 새로운 클립보드를 업데이트 하기 때문에.
-                     **/
-                    int new_len = now_len + now_copied;
-                    int new_sec = now_sec + 1;
-
-                    // 1개 이상 있을 때만 복붙 가능.
-                    if (now_copied > 0 && new_len <= 2000) {
-                        if (new_len == N) {
-                            result = new_sec;
-                            found = true;
-                            break;
-                        }
-                        //pqueue.add(new Clip(new_len, now_copied, now_count + clip_board[now_copied] + 1));
-                        queue.add(new Clip(new_len, now_copied, new_sec));
-                    }
-                } else {
-                    // 3) 1개 지우기
-                    int new_len = now_len - 1;
-                    int new_sec = now_sec + 1;
-
-                    if (new_len > 0) {
-                        if (new_len == N) {
-                            result = new_sec;
-                            found = true;
-                            break;
-                        }
-                        queue.add(new Clip(new_len, now_copied, new_sec));
-                    }
+        // 최악의 상황 : 1 복사(1) -> 1 복붙(1) -> dp[i] == i
+        for (int i = 2; i <= MAX; ++i) dp[i] = i;
+ 
+        for (int start = 2; start < MAX; ++start) {
+            if (dp[start + 1] + 1 < dp[start]) {
+                dp[start] = dp[start + 1] + 1;
+            }
+            
+            int time = dp[start];
+            for (int i = start * 2; i <= MAX; i *= 2) {
+                time += 2;
+                /* 1) 복사하기
+                i = 4(2에서 이미 복사 붙여넣기 완료)
+                */
+                int clip = i / 2;
+                if (time < dp[i]) dp[i] = time;
+                    
+                /* 2) 붙여넣기
+                start = 2 일 때, 
+                a. i = 4 일 때, 2 계속 붙여넣기
+                b. i = 8 일 때, 2 계속 붙여넣기는?
+                -> 4 붙여넣고 2 붙여넣을 수 없잖아. a에서 이미 카운트 됨. 
+                c. i = 8 일 때, 4 계속 붙여넣기
+                */
+                int temp = time;
+                for (int j = i + clip; j <= MAX; j += clip) {
+                    ++temp;
+                    if (temp < dp[j]) dp[j] = temp;
                 }
             }
-
-            if (found) break;
         }
-    }
 
-    static class Clip {
-        int len, copied, sec;
-        //boolean ispasted;
-
-        public Clip(int len, int copied, int sec) {
-            this.len = len;
-            this.copied = copied;
-            this.sec = sec;
+        // 3) -1 할 때가 더 빠르면 그걸로.
+        for (int i = 2; i < MAX; ++i) {
+            if (dp[i + 1] + 1 < dp[i]) {
+                dp[i] = dp[i + 1] + 1;
+            }
         }
-    }
 
+        System.out.println(dp[S]);
+    }
 }
