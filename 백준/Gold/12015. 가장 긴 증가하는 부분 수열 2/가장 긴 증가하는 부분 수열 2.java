313df3c72a390410fddc6e33bb7331d2915c6a25
loveyoun7 <lovekite0406@naver.com>
2023-05-26 17:40:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 524 ms, Memory: 95968 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,51 +1,59 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        /**12015_Algorithm_flow: 그리디(이진 탐색) + 구현
-         **/
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
         int[] arr = new int[N];
-        int[] LIS = new int[N];
+        int[] fake_LIS = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        /** 0부터 넣는 다면, 코드 통일성을 위한 것이다 **/
-        LIS[0] = arr[0];
+        // 초기화
+        fake_LIS[0] = arr[0];
         int lengthOfLIS = 1;
 
         for (int i = 1; i < N; i++) {
-
             int new_element = arr[i];
 
-            // LIS 에 마지막 값보다 클 경우, 추가
-            if (LIS[lengthOfLIS - 1] < new_element) {
+            // fake_LIS 에 마지막 값보다 클 경우, 최장 길이 늘어나고 값 추가
+            if (fake_LIS[lengthOfLIS - 1] < new_element) {
                 lengthOfLIS++;
-                LIS[lengthOfLIS - 1] = new_element;
+                fake_LIS[lengthOfLIS - 1] = new_element;
             } else {
-                /** 삽입할 곳 이분탐색 **/
-                int start = 0;
-                int end = lengthOfLIS;
+                /*
+                10 15 20 30 1 2
+                10 15 20 30(원래 답) -> 1 15 20 30 -> 1 2 15 30(변형 fake 답)
+                
+                10 20 30 1 2 3
+                10 20 30 -> 1 20 30 -> 1 2 30 -> 1 2 (30 -> 3)
+                */
 
-                while (start < end) {
-                    int mid = (start + end) >>> 1;
+                /** 삽입할 곳 이분 탐색 **/
+                int s = 0;
+                int e = lengthOfLIS;
 
-                    // LIS[start] LIS[mid] new_element(포함) LIS[end]
-                    if (LIS[mid] < new_element) start = mid + 1;
-                        // LIS[start] new_element==LIS[mid](포함) LIS[end]
-                    else end = mid;
+                while (s < e) {
+                    // mid = (s + e) / 2;
+                    int mid = (s + e) >>> 1;
+
+                    // fake_LIS[mid] >= new_element : lower bound
+                    if (fake_LIS[mid] < new_element) s = mid + 1;
+                    else e = mid;
                 }
-                //start mid end -> start==mid end -> start == end
-                /** 치환 **/
-                LIS[start] = new_element;
+
+                /*  
+                 new_element <= fake_LIS[s]
+                 fake_LIS fake_LIS[s]  fake_LIS -> fake_LIS  new element  fake_LIS
+                 */
+                // 치환
+                fake_LIS[s] = new_element;
             }
         }
 
