a0cd6247ac2a342412d9347f99e26867b25a18fc
loveyoun7 <lovekite0406@naver.com>
2023-05-27 18:33:17 +0900

    [Gold IV] Title: 테트로미노, Time: 596 ms, Memory: 33320 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -5,25 +5,42 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, max;
-    static int[][] paper, sums;
-    static boolean[][] visited;
-
-    // 동(0), 서(1), 남(2), 북(3)
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
+    static int N, M, max, size;
+    static int[][] paper;
 
     /*
-    북, 동, 서
-    북, 동, 남
-    동, 서, 남
-    서, 남, 북
+     동(0), 서(1), 남(2), 북(3)
+     동동(4), 남남(5)
+     대_1(6), 대_2(7), 대_3(8), 대_4(9)
      */
-    static int[][] fuck_dir = {
+    static int[] dx = {0, 0, 1, -1, 0, 2, -1, -1, 1, 1};
+    static int[] dy = {1, -1, 0, 0, 2, 0, -1, 1, -1, 1};
+    static int[][] tetris = {
+            // ㅣ
+            {1, 0, 4},
+            {3, 2, 5},
+            // ㅁ
+            {0, 2, 9},
+            // ㄴ
+            {3, 2, 9},
+            {3, 2, 8},
+            {1, 0, 7},
+            {1, 0, 6},
+            {6, 3, 2},
+            {7, 3, 2},
+            {8, 1, 0},
+            {9, 1, 0},
+            // ㄹ
+            {1, 2, 9},
+            {0, 2, 8},
+            {3, 1, 8},
+            {3, 0, 9},
+            // ㅗ
             {3, 0, 1},
             {3, 0, 2},
             {0, 1, 2},
-            {1, 2, 3}};
+            {1, 2, 3}
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,8 +48,6 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         paper = new int[N][M];
-        sums = new int[N][M];
-        visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++)
@@ -41,51 +56,23 @@ public class Main {
 
 
         max = 0;
+        size = tetris.length;
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                DFS(i, j, paper[i][j],1);
-                setFuck(i, j);
-            }
+            for (int j = 0; j < M; j++)
+                place4Blocks(i, j);
         }
 
         System.out.println(max);
     }
 
-    static void DFS(int x, int y, int sum, int block) {
-        if (visited[x][y]) return;
-        if (block == 4) {
-            /* 이거 웨 않됌?
-             max = Math.max(max, sums[x][y] + paper[x][y]);
-             */
-            max = Math.max(max, sum);
-            return;
-        }
-
-        visited[x][y] = true;
-        for (int l = 0; l < 4; l++) {
-            int new_x = x + dx[l];
-            int new_y = y + dy[l];
-
-            if (new_x >= 0 && new_x < N && new_y >= 0 && new_y < M) {
-                //sums[new_x][new_y] = sums[x][y] + paper[x][y];
-
-                DFS(new_x, new_y, sum + paper[new_x][new_y], block + 1);
-
-                //sums[new_x][new_y] = 0;
-            }
-        }
-
-        visited[x][y] = false;
-    }
-
-    static void setFuck(int x, int y) {
-        for (int k = 0; k < 4; k++) {
+    static void place4Blocks(int x, int y) {
+        for (int k = 0; k < size; k++) {
             int sum = paper[x][y];
             boolean impossible = false;
 
             for (int l = 0; l < 3; l++) {
-                int new_x = x + dx[fuck_dir[k][l]];
-                int new_y = y + dy[fuck_dir[k][l]];
+                int new_x = x + dx[tetris[k][l]];
+                int new_y = y + dy[tetris[k][l]];
 
                 if (new_x >= 0 && new_x < N && new_y >= 0 && new_y < M)
                     sum += paper[new_x][new_y];
