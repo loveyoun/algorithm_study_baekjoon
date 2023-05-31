ee967532f730f0963aa69600e3c661b081a723ac
loveyoun7 <lovekite0406@naver.com>
2023-05-31 15:56:19 +0900

    [Gold V] Title: 리모컨, Time: 296 ms, Memory: 64560 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,115 +1,43 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine()); // 보려는 채널
+        int M = Integer.parseInt(br.readLine()); // 고장난 버튼 개수 M
+        int min = Math.abs(100 - N);
 
-        //이동하려는 채널 N
-        String N = br.readLine();
-        int n = Integer.parseInt(N);
-        /*int[] narr = new int[N.length()]; //~=null nullPointException
-        for(int i=0;i<N.length();i++)
-            narr[i] = N.charAt(i)-'0'; //~N[i]^^ N.charAt(i)*/
-
-        //가장 가까운 수 중 큰 수, 작은 수
-        int max = n, min = n;
-        int maxlen=N.length(), minlen=N.length();
-        boolean outoforder = false;
-
-        //고장난 버튼 개수 M
-        int M = Integer.parseInt(br.readLine());
-        //고장난게 없을 때
-        if(M==0) {
-            System.out.println(N.length() > Math.abs(100-n) ? Math.abs(100-n) : N.length());
+        /* Null Pointer */
+        if (M == 0) {
+            System.out.println(Math.min(min, Integer.toString(N).length()));
             return;
         }
-        //다 고장났을 때
-        if(M==10){
-            System.out.println(Math.abs(100-n));
-            return;
-        }
-
         String[] tmp = br.readLine().split(" ");
-        int[] broken = new int[M];
-        for(int i=0;i<M;i++) broken[i] = Integer.parseInt(tmp[i]);
+        boolean[] broken = new boolean[10]; // 0 ~ 9
+        for (int i = 0; i < M; i++) broken[Integer.parseInt(tmp[i])] = true;
 
 
-        //큰 수 찾기
-        while(true){
-            N = Integer.toString(max); maxlen=N.length();
-            for(int i=0;i<maxlen;i++){
-                for(int b : broken){
-                    /*System.out.print(b+"\\");
-                    System.out.println(N.charAt(i));*/
-                    //고장나서 못 누르면
-                    if((N.charAt(i)-'0') == b) {
-                        outoforder = true;
-                        break;
-                    }
+        for (int num = 0; num < 1000000; num++) {
+            boolean cantReach = false;
+
+            String snum = Integer.toString(num); //String.valueOf(num);
+            int len = snum.length();
+
+            for (int i = 0; i < len; i++) {
+                if (broken[snum.charAt(i) - '0']) {
+                    cantReach = true;
+                    break;
                 }
-                if(outoforder) break;
-            }
-            //안고장난 버튼들 이면
-            if(!outoforder) break;
-            /*한없이 끝까지 커질 때
-            n = 0 -> max = ...99
-            500,000 -> 511,111*/
-            if((max-n+maxlen)>Math.abs(100-n)) {
-                //max = 1000001; // n-100 < max-n+maxlen 이어야 함.
-                break;
             }
 
-            max+=1;
-            outoforder = false;
+            if (!cantReach) min = Math.min(min, (len + Math.abs(N - num)));
         }
-        maxlen=N.length();
-        outoforder = false;
-
-        /**변수 재활용 조심하기**/
-        //작은 수 찾기
-        while(true){
-            N = Integer.toString(min); minlen=N.length();
-            for(int i=0;i<minlen;i++){
-                for(int b : broken){
-                    /*System.out.print(b+"\\");
-                    System.out.println(N.charAt(i));*/
-                    //고장나서 못 누르면
-                    if((N.charAt(i)-'0') == b) {
-                        outoforder = true;
-                        break;
-                    }
-                }
-                if(outoforder) break;
-            }
-            //안고장난 버튼들 이면
-            if(!outoforder) break;
-            //한없이 작아질 때
-            if(min<=0) {
-                min = n+minlen-Math.abs(100-n); //n-100 < n-min+minlen 이어야 함.
-                break;
-            }
 
 
-            min-=1;
-            outoforder = false;
-        }
-        minlen=N.length();
-
-
-        int MAX = max-n+maxlen;
-        int MIN = n-min+minlen;
-        int semi = MAX > MIN ? MIN : MAX;
-        int result = semi > Math.abs(100-n) ? Math.abs(100-n) : semi;
-        System.out.println(result);
-//        bw.write(result);
-//        bw.flush();
-//        bw.close();
+        System.out.println(min);
     }
 
 }
