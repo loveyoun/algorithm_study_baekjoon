8a5bce01cdb88983eb3800fe75583de866bda9f3
loveyoun7 <lovekite0406@naver.com>
2023-06-04 00:04:08 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M, L;
-    static int[] rest_locs, locs_diff;
+    static int[] rest_locs, locs_diffs;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,32 +16,47 @@ public class Main {
         M = Integer.parseInt(st.nextToken()); // 미래
         L = Integer.parseInt(st.nextToken()); // 총 거리
         rest_locs = new int[N + 1];
-        locs_diff = new int[N + 1];
+        locs_diffs = new int[N + 1];
 
-        /* 극단 케이스 핸들링_1
+        /* 
+        극단 케이스 핸들링_1 : 최댓값이 아니라, 가능한 최댓값 중 최솟값이다
         if (N == 0) {
             // 1 2 3 4 ~ L
-            //최댓값이 아니라, 최댓값 중 최솟값이다
             System.out.println(L - M + 1);
             return;
         }
         */
 
-        if(N>0)st = new StringTokenizer(br.readLine());
+        if (N > 0) st = new StringTokenizer(br.readLine());
         rest_locs[0] = L;
         for (int i = 1; i <= N; i++)
             rest_locs[i] = Integer.parseInt(st.nextToken());
         Arrays.sort(rest_locs);
 
-        /*locs_diff[0] = rest_locs[0] - 1;*/
-        locs_diff[0] = rest_locs[0];
+        /*
+         locs_diff[0] = rest_locs[0] - 1;
+         locs_diff[0] = 7 -> mid 가 7일 때 7 / 7 = 1 - 1 로 안 되고, 6일 때 1, locs_diff[0] 으로 가능하다. 
+         
+         마찬가지로, locs_diff[N] = L - rest_locs[N-1] - 1 이 아닌 이유이다.
+         L = 10, rest_locs[N-1] = 5 일 때, 5 / 5 = 1 - 1 로 안된다.
+         L 에 놓이면 안 되므로, 거리가 4인 9 지점에 놓여야 한다.
+         */
+        locs_diffs[0] = rest_locs[0];
         for (int i = 1; i <= N; i++)
-            locs_diff[i] = rest_locs[i] - rest_locs[i - 1];
-        Arrays.sort(locs_diff);
-        
+            locs_diffs[i] = rest_locs[i] - rest_locs[i - 1];
+        Arrays.sort(locs_diffs);
 
-        int result = binarySearch(1, locs_diff[N]); //
-        /*while (last_loc + result == L) result--;*/
+
+        /* 
+         low = 0이면 에러
+         거리가 1과 locs_diffs[N] 일때를 포함한다.
+         (하지만 locs_diffs[N] == L 일 때는 L-1 까지 고려되게 이분 탐색을 구성하여야 한다.)
+         */
+        int result = binarySearch(1, locs_diffs[N]);
+        /*
+         극단 케이스 핸들링_2 : 여기서 해주면 안 된다. 방식도 틀렸다.
+         while (last_loc + result == L) result--;
+         */
         System.out.println(result);
     }
 
@@ -51,9 +66,10 @@ public class Main {
 
             int count = 0;
             for (int i = N; i >= 0; i--) {
-                int tmp = locs_diff[i] / mid;
-                if (locs_diff[i] % mid == 0) tmp--;
+                int tmp = locs_diffs[i] / mid;
+                if (locs_diffs[i] % mid == 0) tmp--;
                 count += tmp;
+
                 if (tmp == 0) break;
             }
 
