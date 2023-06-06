da9ee93acdf06f4dd75384f229fc3a640644be49
loveyoun7 <lovekite0406@naver.com>
2023-06-06 23:48:52 +0900

    [Gold IV] Title: 사촌, Time: 652 ms, Memory: 150152 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.java
+++ 백준/Gold/9489. 사촌/사촌.java
@@ -7,8 +7,8 @@ public class Main {
 
     static StringTokenizer st;
     static StringBuilder sb;
-    static int n, k, level, k_idx, k_level, k_group, k_siblings, p;
-    static int[] tree, level_count, level_group, parent;
+    static int n, k, k_idx, k_siblings, p, level;
+    static int[] tree, parent, level_count, level_group;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,22 +20,22 @@ public class Main {
             if (n == 0) break;
             k = Integer.parseInt(st.nextToken());
 
-            tree = new int[n + 1];
+            tree = new int[n + 1]; // 루트 노드(1) ~ n번 까지, 모두 n 명
             parent = new int[n + 1];
-            level_count = new int[n + 1];
-            level_group = new int[n + 1];
+            /* group + level -> parent 정보로 한 번에
+             level_count = new int[n + 1]; // 같은 레벨에 있는 친척 사람 수
+             level_group = new int[n + 1]; // 같은 레벨에 있는 형제 그룹 수
 
-            level_count[1] = 1;
-            level_group[1] = 1;
+             level_count[1] = 1;
+             level_group[1] = 1;
+             level = 1;
+             */
             p = 0;
             parent[1] = p; // root
 
-            st = new StringTokenizer(br.readLine());
-
-            level = 1;
             k_idx = 0;
-            k_level = 0;
-            k_group = 0;
+
+            st = new StringTokenizer(br.readLine());
             findCousins();
         }
 
@@ -48,22 +48,31 @@ public class Main {
         for (int i = 2; i <= n; i++) {
             tree[i] = Integer.parseInt(st.nextToken());
 
-            // 자식이나 사촌
+            // 다음 세대나 사촌
             if (tree[i] > tree[i - 1] + 1) {
-                // 자식
-                if (level_count[level - 1] < level_group[level] + 1) level++;
+                // 다음 세대로 넘어갈 때
+                /* if (level_count[level - 1] < level_group[level] + 1) level++; */
 
                 // 사촌
+                /*
+                 p++;
+                 level_count[level]++;
+                 level_group[level]++;
+                 */
+
+                // 레벨 사이의 경계인지 확인
+                /*if (parent[i - 1] != parent[parent[i - 1] + 1])*/
                 p++;
-                level_count[level]++;
-                level_group[level]++;
             }
-            // 형제
+            /* 형제 수 필요 없어짐
             else level_count[level]++;
+             */
 
             parent[i] = p;
+
             if (tree[i] == k) k_idx = i;
         }
+
 
         k_siblings = 0;
         for (int i = 2; i <= n; i++) {
@@ -72,7 +81,7 @@ public class Main {
             if (p1 != p2 && parent[p1] == parent[p2]) k_siblings++;
         }
 
-        sb.append(k_siblings + "\n");
+        sb.append(k_siblings).append("\n");
     }
 
 }
