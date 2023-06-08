f5cc4ee10d1f42241b71bb7926e93337d3419012
loveyoun7 <lovekite0406@naver.com>
2023-06-08 23:20:15 +0900

    [Gold III] Title: 종이 조각, Time: 400 ms, Memory: 20224 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.java
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static int[][] nums;
+    public static int[][] paper;
     public static int[][] sums;
     public static int[][] xs;
     public static int[][] ys;
     public static boolean[][] visited;
-    public static int N, M;
-    public static int count, sum, max;
+    public static int N, M; // 행, 열
+    public static int count, sum, result;
 
     public static void main(String[] args) throws IOException {
-        /**14391_Algorithm flow: **/
+        /** 세로 1, 2, .. 가로 1, 2, .. '마다' 재귀 (바로 다음 인덱스 세로 1, 2.. 가로 1, 2..(...)) **/
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] sarr = br.readLine().split(" ");
-        N = Integer.parseInt(sarr[0]); //행
-        M = Integer.parseInt(sarr[1]); //열
+        N = Integer.parseInt(sarr[0]);
+        M = Integer.parseInt(sarr[1]);
+        paper = new int[N][M];
 
-        nums = new int[N][M];
         sums = new int[N][M];
         xs = new int[N][M];
         ys = new int[N][M];
@@ -29,89 +29,81 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
-            for (int j = 0; j < M; j++) {
-                nums[i][j] = s.charAt(j) - '0';
-            }
+            for (int j = 0; j < M; j++) paper[i][j] = s.charAt(j) - '0';
         }
 
         count = 0;
         sum = 0;
-        max = 0;
-        rightdown(0, 0);
+        result = 0;
+        right_down(0, 0); // 오른쪽으로 자르고, 아래쪽으로 자르고
 
-        System.out.println(max);
+        System.out.println(result);
     }
 
 
-    public static void rightdown(int n, int m) {
-        if (!visited[n][m]) { //visited 안 한 점이면
-            for (int i = 0; i < N - n; i++) {
+    public static void right_down(int n, int m) {
+        if (!visited[n][m]) {
+            int v = 0, h = 0;
+            for (int i = 0; i < N - n; i++) { // ex> N = 4, n = 2 -> 2, 3
                 for (int j = 0; j < M - m; j++) {
-                    if (i != 0) { //세로탐색
-                        for (int l = 0; l <= i; l++) { //[n][m] ~ [n+i][m]까지 더하기
-                            /**!!!visited[n+l][m]도 false여야 함!!!**/
-                            if(!visited[n+l][m]){
-                                sums[n][m] = sums[n][m] * 10 + nums[n + l][m];
+                    // 1. 아래, 오른쪽 탐색
+                    if (i != 0) { // 세로탐색
+                        for (v = 0; v <= i; v++) { // [n][m] ~ [n+i][m] 까지 더하기
+                            if (!visited[n + v][m]) {
+                                sums[n][m] = sums[n][m] * 10 + paper[n + v][m];
                                 count++;
-                                visited[n + l][m] = true;
-                                xs[n][m]=l;
-                            }else break;
+                                visited[n + v][m] = true;
+                                xs[n][m] = v;
+                            } else break;
                         }
-                    } else { //i==0일 때. 가로탐색
-                        for (int l = 0; l <= j; l++) { //[n][m] ~ [n][m+j]까지 더하기
-                            if(!visited[n][m+l]){
-                                sums[n][m] = sums[n][m] * 10 + nums[n][m + l];
+                    } else { // i == 0 일 때. 단독 or 가로탐색
+                        for (h = 0; h <= j; h++) { // [n][m] ~ [n][m+j] 까지 더하기
+                            if (!visited[n][m + h]) {
+                                sums[n][m] = sums[n][m] * 10 + paper[n][m + h];
                                 count++;
-                                visited[n][m + l] = true;
-                                ys[n][m]=l;
+                                visited[n][m + h] = true;
+                                ys[n][m] = h;
                             } else break;
                         }
                     }
 
-                    if (count == N * M) countcheck(); //count==16 점검
-                    //countcheck()했으면, count==16이고, 나머지 다 visited니까 재귀함수 시작 안할 것.
+                    // 2. 전체 종이조각 돌았을 때 max 값 업데이트
+                    if (count == N * M) iterCheck();
 
-                    /**!!!!!근데, 이건 처음 visited 상관없이 실행되어야하는데!!!!!**/
+                    // 3. 그 다음 인덱스 (오른쪽으로 한 칸씩) 재귀
                     if ((m + 1) >= M) {
-                        if ((n + 1) < N) rightdown(n + 1, 0);
-                    } else rightdown(n, m + 1);
+                        if ((n + 1) < N) right_down(n + 1, 0);
+                    } else right_down(n, m + 1);
 
-                    /**재귀 끝나면 리셋해줄 코드**/
+                    // 4. 이 단계에서 해준거 고대로 리와인드
                     sums[n][m] = 0;
-                    if (i != 0) { //세로탐색
-                        for (int l = 0; l <= xs[n][m]; l++) {
+                    if (i != 0) { // 세로탐색
+                        for (int l = 0; l < v; l++) {
                             count--;
                             visited[n + l][m] = false;
                         }
-                    } else { //i==0일 때. 가로탐색
-                        for (int l = 0; l <= ys[n][m]; l++) {
+                    } else { // 가로탐색
+                        for (int l = 0; l < h; l++) {
                             count--;
                             visited[n][m + l] = false;
                         }
                     }
-                }//안쪽 for
-            }//바깥 for
-        }//if문
-        else { //visited 한 점이면, 다음 인덱스로...
-            /**if~else 중첩일 때 조심...**/
-            if ((m + 1) >= M) {//열 끝났는데
-                //행안 끝났으면, 다음 행 0열로 가야함.
-                if ((n + 1) < N) rightdown(n + 1, 0);
-            } else rightdown(n, m + 1);
+                } // 안쪽 for
+            } // 바깥 for
+        } else { // if (visited)
+            // 행 다 탐색 후, 다음 열로 돌아갈 때 : ex> (1, M - 1) ->  (2, 0)
+            if ((m + 1) >= M) {
+                if ((n + 1) < N) right_down(n + 1, 0);
+            } else right_down(n, m + 1);
         }
     }
 
-    //(N * M)개 index 다 돌았는지
-    public static void countcheck() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                sum += sums[i][j];
-                //System.out.print(sums[i][j] + " ");
-            }
-            //System.out.println("");
-        }
-        max = Math.max(max, sum);
-        //System.out.println(max+"\n");
+    public static void iterCheck() {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) sum += sums[i][j];
+
+        result = Math.max(result, sum);
+
         sum = 0;
     }
 
