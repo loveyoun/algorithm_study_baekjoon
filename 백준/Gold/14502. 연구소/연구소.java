d4d1b7bed6f5d43f95b4c66cec232f58c5b55663
loveyoun7 <lovekite0406@naver.com>
2023-06-10 19:34:15 +0900

    [Gold IV] Title: 연구소, Time: 352 ms, Memory: 126808 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,43 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M, stabled, stable;
-    static int[][] lab, infected;
-    static boolean[][] visited;
-    static int max;
+    static int N, M, safety_z, tmp_safe_z, max, wallCount;
+    static int[][] lab;
+    static boolean[][] vi_visited;
     static int[] dx = {0, -1, 0, 1};
     static int[] dy = {-1, 0, 1, 0};
+    static ArrayList<Point> virus;
+    static Queue<Point> queue;
 
     public static void main(String[] args) throws IOException {
-        /** 14502_Algorithm_연구소 :
-         **/
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        stabled = 0;
+        safety_z = -3; // 벽 미리 카운트
 
         lab = new int[N][M];
-        infected = new int[N][M];
+        vi_visited = new boolean[N][M];
+        virus = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 lab[i][j] = Integer.parseInt(st.nextToken());
-                if (lab[i][j] == 0) stabled++;
+                if (lab[i][j] == 0) safety_z++; // 한 iteration 후, 안전 지역을 카운트하는 이중 for 문 대신 사용
+                if (lab[i][j] == 2) {
+                    vi_visited[i][j] = true; // 전염시킨 바이러스 라는 표시
+                    virus.add(new Point(i, j));
+                }
             }
         }
-        stabled -= 3;
 
 
         max = 0;
+        wallCount = 0;
         makeWall();
 
         System.out.println(max);
@@ -68,21 +72,20 @@ public class Main {
                                         if (lab[n][m] == 0) {
                                             lab[n][m] = 1;
 
-                                            visited = new boolean[N][M];
-                                            infected = new int[N][M];
-                                            stable = stabled;
+                                            vi_visited = new boolean[N][M];
+                                            tmp_safe_z = safety_z;
                                             BFS();
-                                            max = Math.max(max, stable);
+                                            max = Math.max(max, tmp_safe_z);
 
                                             lab[n][m] = 0;
                                         }
                                         m++;
-                                    } //m = 0; 무한루프
+                                    } /* m = 0; 무한루프 */
                                 } // 세 번째 벽
                                 lab[x][y] = 0;
                             }
                             y++;
-                        } //y = 0; 무한루프
+                        } /* y = 0; 무한루프 */
                     } // 두 번째 벽
                     lab[i][j] = 0;
                 }
@@ -91,52 +94,35 @@ public class Main {
     }
 
     static void BFS() {
-        Queue<Point> queue = new LinkedList<>();
+        queue = new LinkedList<>();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (lab[i][j] == 2 || infected[i][j] == 2) {
-                    if (!visited[i][j]) {
-                        infected[i][j] = 2;
-                        visited[i][j] = true;
-                        queue.add(new Point(i, j));
+        // 기존 바이러스 구역으로 초기화
+        for (Point point : virus) queue.add(point);
 
-                        while (!queue.isEmpty()) {
-                            Point now = queue.poll();
-                            int x = now.x;
-                            int y = now.y;
+        // queue 에 기존 바이러스 구역을 다 넣고, 전염시키면 된다.
+        // 어차피 한 iteration 당 BFS 한 번만 도니까
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+            int x = now.x;
+            int y = now.y;
 
-                            for (int k = 0; k < 4; k++) {
-                                int new_x = x + dx[k];
-                                int new_y = y + dy[k];
+            for (int k = 0; k < 4; k++) {
+                int new_x = x + dx[k];
+                int new_y = y + dy[k];
 
-                                if (new_x >= 0 && new_x < N && new_y >= 0 && new_y < M) {
-                                    // lab: 기존 바이러스(2), 기존 안전(0), 기존 벽 + 새로운 벽(1)
-                                    // infected: 새로운 바이러스(2)
-                                    if (lab[new_x][new_y] == 0 && infected[new_x][new_y] == 0) {
-                                        infected[new_x][new_y] = 2;
-                                        visited[new_x][new_y] = true;
-                                        stable--;
-                                        queue.add(new Point(new_x, new_y));
-                                    }
-                                }
-                            }
-                        } //while (!queue.isEmpty())
-                    } //if (!visited[i][j])
-                } //if (infected[i][j] == 2 && !visited[i][j])
-            }
+                if (new_x >= 0 && new_x < N && new_y >= 0 && new_y < M) {
+                    if (lab[new_x][new_y] == 0 && !vi_visited[new_x][new_y]) {
+                        vi_visited[new_x][new_y] = true;
+                        /* lab[new_x][new_y] = 2; */
+                        tmp_safe_z--;
+
+                        queue.add(new Point(new_x, new_y));
+                    }
+                }
+            } // 동서남북 탐색
         }
-    }
 
-    /*
-    시간이 너무 걸린다. 갯수로 해결해보자 -> int stable
-    static void findStable(){
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-            }
-        }
     }
-     */
 
     static class Point {
         int x, y;
