ff402a89af1c1b28ab7c1c0968884cb00b5f0f59
loveyoun7 <lovekite0406@naver.com>
2023-06-12 21:50:08 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1112 ms, Memory: 314752 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M, K;
     static char[][] map;
-    static boolean[][][] visited;
+    static int[][] visited;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
 
@@ -20,26 +22,27 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
         map = new char[N][M];
         for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
-        visited = new boolean[K + 1][N][M]; // 0 : 정상, 1 ~ K 번 깨짐
+        visited = new int[N][M]; // 0 : 정상, 1 ~ K 번 깨짐
+        for (int i = 0; i < N; i++) Arrays.fill(visited[i], 11);
 
-        System.out.println(BFS());
+        if (N == 1 && M == 1) System.out.println(1);
+        else System.out.println(BFS());
     }
 
     static int BFS() {
-        PriorityQueue<Point> queue = new PriorityQueue<>((o1, o2) -> {
-            return o1.path - o2.path;
-        });
+        //PriorityQueue<Point> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.path));
+        Queue<Point> queue = new LinkedList<>();
 
-        visited[0][0][0] = true;
+        visited[0][0] = 0;
         queue.add(new Point(0, 0, 1, 0));
 
         while (!queue.isEmpty()) {
             Point now = queue.poll();
             int cur_x = now.x;
             int cur_y = now.y;
-            int path = now.path;
-            if (cur_x == N - 1 && cur_y == M - 1) return path;
-            /*if (wallCount + 1 > K) continue;*/
+            int path = now.path + 1;
+            // Break Point
+            /*if (cur_x == N - 1 && cur_y == M - 1) return path;*/
 
             for (int k = 0; k < 4; k++) {
                 int new_x = cur_x + dx[k];
@@ -50,18 +53,20 @@ public class Main {
                 // 벽'1' 만났을 때 하나 더 꺨 수 있으니까 깨고
                 if (map[new_x][new_y] == '1') wallCount++;
                 if (wallCount > K) continue;
-                if (visited[wallCount][new_x][new_y]) continue;
-                // '0'
-                /* 3번째 벽 깰 차례인데 이미 1 or 2에서 깨버렸으면 : x
-                 -> 1, 2가 틀리면 3으로 가야함. 0이면 괜찮음
-                if (visited[new_x][new_y] > 0 && visited[new_x][new_y] < wallCount) continue;
-                */
-                /* wallCount 번 깬 루트이다.
-                visited[new_x][new_y] = wallCount;
-                다른 루트가 갱신할 수 있다.
+
+                /* '0'
+                 3번째 벽 깰 차례인데 이미 1 or 2에서 깨버렸으면 : x
+                 -> 1, 2가 틀리면 3으로 가야함. 0이면 괜찮음 : x
+                 -> 1, 2 가 안되면 어차피 3도 안될 것. 더 꺨 수 있는 적은 수로 업데이트
+                 벽을 던 깬걸로 업데이트 해주는 게 . 그 루트가 실패할 수도 있고
+                 실패하면 더 벽을 깰 수 있는 걸로 대체되어야함. 더 안전한 루트
+                 if (visited[new_x][new_y] <= wallCount) continue;
                  */
-                visited[wallCount][new_x][new_y] = true;
-                queue.add(new Point(new_x, new_y, path + 1, wallCount));
+                if (visited[new_x][new_y] > wallCount) visited[new_x][new_y] = wallCount; // wallCount 번 깬 루트이다
+                else continue; // <=
+
+                if (new_x == N - 1 && new_y == M - 1) return path;
+                queue.add(new Point(new_x, new_y, path, wallCount));
             }
         }
 
