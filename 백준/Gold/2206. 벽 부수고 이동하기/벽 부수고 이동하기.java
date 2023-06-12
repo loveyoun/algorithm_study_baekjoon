a94da5360194d768f77e5fc3e997cda56e8fd39a
loveyoun7 <lovekite0406@naver.com>
2023-06-12 15:26:21 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 536 ms, Memory: 121688 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -9,63 +9,90 @@ public class Main {
 
     static int N, M;
     static char[][] map;
-    static boolean[][][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] org_visited, brk_visited;
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         map = new char[N][M];
-        visited = new boolean[2][N][M]; // 0: not_broken, 1: broken
+        org_visited = new boolean[N][M];
+        brk_visited = new boolean[N][M];
+        for (int i = 0; i < N; i++)
+            map[i] = br.readLine().toCharArray();
 
-        for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
 
-        System.out.println(BFS());
+       System.out.println(BFS());
     }
 
-    private static int BFS() {
-        Queue<Point> q = new LinkedList<>();
+    static int BFS() {
+        /** path 를 인자로 넘기거나, 배열에 저장할 수도 있다 **/
 
-        visited[0][0][0] = true;
-        q.add(new Point(0, 0, 1, 0));
+        Queue<Point> queue = new LinkedList<>();
 
-        while (!q.isEmpty()) {
-            Point now = q.poll();
-            int now_x = now.x;
-            int now_y = now.y;
-            int path = now.path;
-            if (now_x == N - 1 && now_y == M - 1) return path;
+        org_visited[0][0] = true;
+        queue.add(new Point(0, 0, 1, false));
+
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+            int cur_x = now.x;
+            int cur_y = now.y;
+            int cur_path = now.path; // 현재까지 거리
+            // Break Point
+            if (cur_x == N - 1 && cur_y == M - 1)
+                return cur_path;
 
             for (int i = 0; i < 4; i++) {
-                int new_x = now_x + dx[i];
-                int new_y = now_y + dy[i];
-                int isBroken = now.isBroken;
+                int new_x = cur_x + dx[i];
+                int new_y = cur_y + dy[i];
+                boolean isBroken = now.broken;
                 if (new_x < 0 || new_x >= N || new_y < 0 || new_y >= M) continue;
 
-                // visited 여부, 벽_'1', 정_'1'
-                if (visited[isBroken][new_x][new_y] || (map[new_x][new_y] == '1' && isBroken == 1)) continue;
-                if (map[new_x][new_y] == '1' && isBroken == 0) isBroken = 1;
 
-                // visited 처리, 벽_'0', 정_'0' 
-                visited[isBroken][new_x][new_y] = true;
-                q.add(new Point(new_x, new_y, path + 1, isBroken));
-            }
-        }
+                // visited 여부, 벽 만났을 때, visited 처리
+                /** 방법 1)
+                if (isBroken && (map[new_x][new_y] == '1' || brk_visited[new_x][new_y])) continue;
+                if (!isBroken && org_visited[new_x][new_y]) continue;
+                if (!isBroken) org_visited[new_x][new_y] = true;
+                if (map[new_x][new_y] == '1') isBroken = true;
+                 **/
+
+                /** 방법 2) **/
+                // 벽 뚫은 경우
+                if (isBroken) {
+                    if (map[new_x][new_y] == '1') continue;
+                    if (brk_visited[new_x][new_y]) continue;
+                }
+                // 정상 루트
+                if (!isBroken) {
+                    if (org_visited[new_x][new_y]) continue;
+                    org_visited[new_x][new_y] = true;
+                    if (map[new_x][new_y] == '1') isBroken = true;
+                }
+
+
+                brk_visited[new_x][new_y] = true; // 벽을 안 뚫어도 정상적으로 미리 도착한게 더 빠르니까
+                queue.add(new Point(new_x, new_y, cur_path + 1, isBroken)); /** 항상 visited 여부 조심! **/
+            } // 4 방위 탐색
+        } // while (queue)
 
         return -1;
     }
 
     static class Point {
-        int x, y, path, isBroken;
+        int x, y, path;
+        boolean broken;
 
-        public Point(int x, int y, int path, int isBroken) {
+        Point(int x, int y, int path, boolean broken) {
             this.x = x;
             this.y = y;
             this.path = path;
-            this.isBroken = isBroken;
+            this.broken = broken;
         }
     }
+
 }
