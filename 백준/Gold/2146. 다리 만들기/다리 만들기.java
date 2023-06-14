7d26dd6ea801210390a838e7ca6c546a431fe415
loveyoun7 <lovekite0406@naver.com>
2023-06-14 13:55:28 +0900

    [Gold III] Title: 다리 만들기, Time: 220 ms, Memory: 51728 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -8,8 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, Is, min;
-    static char is;
-    static char[][] nation;
+    static int[][] nation;
     static boolean[][] visited;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
@@ -19,13 +18,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        nation = new char[N][N];
+        N = read();
+        nation = new int[N][N];
         visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++)
-                nation[i][j] = st.nextToken().charAt(0); /*br.readLine().split(" ").toString().toCharArray();*/
+                nation[i][j] = read(); //st.nextToken().charAt(0); /*br.readLine().split(" ").toString().toCharArray();*/
         }
 
         // 섬 나누기
@@ -33,7 +31,7 @@ public class Main {
         dist = new int[N][N]; // 한 번만 해줘도 됨.
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (visited[i][j] || nation[i][j] == '0') continue;
+                if (visited[i][j] || nation[i][j] == 0) continue;
                 divideIslands(i, j);
             }
         }
@@ -55,16 +53,14 @@ public class Main {
     }
 
     static void divideIslands(int x, int y) {
-        Is++;
-        is = (char) (Is + '0');
-
         int_que = new LinkedList<>();
 
-        nation[x][y] = is;
+        Is++;
+        nation[x][y] = Is;
         visited[x][y] = true;
         int_que.add(new int[]{x, y});
-        brg_queue.add(new Bridge(x, y, 0, is));
-        dist[x][y] = is;
+        brg_queue.add(new Bridge(x, y, 0, Is));
+        dist[x][y] = Is;
 
         while (!int_que.isEmpty()) {
             int[] node = int_que.poll();
@@ -76,13 +72,13 @@ public class Main {
                 int new_y = now_y + dy[k];
 
                 if (new_x < 0 || new_x >= N || new_y < 0 || new_y >= N) continue;
-                if (visited[new_x][new_y] || nation[new_x][new_y] == '0') continue;
+                if (visited[new_x][new_y] || nation[new_x][new_y] == 0) continue;
 
-                nation[new_x][new_y] = is;
+                nation[new_x][new_y] = Is;
                 visited[new_x][new_y] = true;
                 int_que.add(new int[]{new_x, new_y});
-                brg_queue.add(new Bridge(new_x, new_y, 0, is));
-                dist[new_x][new_y] = is;
+                brg_queue.add(new Bridge(new_x, new_y, 0, Is));
+                dist[new_x][new_y] = Is;
             }
         }
     }
@@ -92,7 +88,7 @@ public class Main {
             Bridge brg = brg_queue.poll();
             int now_x = brg.x;
             int now_y = brg.y;
-            char IsNum = brg.IsNum; // 시작 섬
+            int IsNum = brg.IsNum; // 시작 섬
             int path = brg.path;
 
             for (int k = 0; k < 4; k++) {
@@ -108,7 +104,7 @@ public class Main {
                 if (num == IsNum) continue;
 
                 // 바다나 다른 섬 만날 때
-                if (num == '0') {
+                if (num == 0) {
                     dist[new_x][new_y] = IsNum;
                     brg_queue.add(new Bridge(new_x, new_y, path + 1, IsNum));
                 } else {
@@ -121,14 +117,22 @@ public class Main {
 
     static class Bridge {
         int x, y, path;
-        char IsNum;
+        int IsNum;
 
-        Bridge(int x, int y, int path, char IsNum) {
+        Bridge(int x, int y, int path, int IsNum) {
             this.x = x;
             this.y = y;
             this.path = path;
             this.IsNum = IsNum;
         }
+    }
+
+    static int read() throws IOException {
+        // System.in.read() : 수 하나 씩
+        // ex> 31 -> 51, 49 -> 3 * 10 + 1
+        int c, n = System.in.read() & 15; // 1111 & 110000(48) ~ 111010(57),
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15); // 스페이스(32), LF(10), CR(13)
+        return n;
     }
 
 }
