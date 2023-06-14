89e96afe1ac089b5684e02543618dd6f177fbd97
loveyoun7 <lovekite0406@naver.com>
2023-06-14 15:04:44 +0900

    [Gold III] Title: 다리 만들기, Time: 152 ms, Memory: 15228 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -4,12 +4,13 @@ import java.util.Queue;
 
 public class Main {
 
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int N, Is, min;
     static int[][] nation;
     //static boolean[][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] occupied;
+    //static int[][] occupied;
+    static int[][] dist;
     static Queue<int[]> int_que;
     static Queue<Bridge> brg_queue = new LinkedList<>();
 
@@ -23,7 +24,6 @@ public class Main {
 
         // 섬 나누기
         Is = 1;
-        occupied = new int[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (nation[i][j] != 1) continue;
@@ -34,6 +34,7 @@ public class Main {
         // 다리 놓기 4방향 계속 진행 dfs
         // bfs? min 값 업데이트
         min = Integer.MAX_VALUE;
+        dist = new int[N][N];
         setBridge();
 
         System.out.println(min);
@@ -42,11 +43,9 @@ public class Main {
     static void divideIslands(int x, int y) {
         int_que = new LinkedList<>();
 
-        Is++;
-        nation[x][y] = Is;
+        nation[x][y] = ++Is;
         int_que.add(new int[]{x, y});
-        brg_queue.add(new Bridge(x, y, 0, Is));
-        occupied[x][y] = Is;
+        brg_queue.add(new Bridge(x, y,  Is));
 
         while (!int_que.isEmpty()) {
             int[] node = int_que.poll();
@@ -62,8 +61,7 @@ public class Main {
 
                 nation[new_x][new_y] = Is;
                 int_que.add(new int[]{new_x, new_y});
-                brg_queue.add(new Bridge(new_x, new_y, 0, Is));
-                occupied[new_x][new_y] = Is;
+                brg_queue.add(new Bridge(new_x, new_y,  Is));
             }
         }
     }
@@ -73,8 +71,8 @@ public class Main {
             Bridge brg = brg_queue.poll();
             int now_x = brg.x;
             int now_y = brg.y;
-            int IsNum = brg.IsNum; // 시작 섬 번호 
-            int path = brg.path;
+            int IsNum = brg.IsNum; // 시작 섬 번호
+            int path = dist[now_x][now_y];
 
             for (int k = 0; k < 4; k++) {
                 int new_x = now_x + dx[k];
@@ -82,30 +80,31 @@ public class Main {
                 if (new_x < 0 || new_x >= N || new_y < 0 || new_y >= N) continue;
                 int num = nation[new_x][new_y];
 
-                // 이미 다리놨거나 같은 섬이면
-                if (occupied[new_x][new_y] == IsNum) continue;
+                // 이미 다리 놨거나 같은 섬이면
                 if (num == IsNum) continue;
 
                 // 바다나 다른 섬 만날 때
                 if (num == 0) {
-                    occupied[new_x][new_y] = IsNum;
-                    brg_queue.add(new Bridge(new_x, new_y, path + 1, IsNum));
+                    dist[new_x][new_y] = path + 1;
+                    nation[new_x][new_y] = IsNum; /****/
+                    brg_queue.add(new Bridge(new_x, new_y, IsNum));
                 } else {
-                    min = Math.min(min, path);
-                    return; // BFS 니까
+                    min = Math.min(min, path + dist[new_x][new_y]);
+                    /* 시작점이 다른 BFS 주의
+                    return;
+                     */
                 }
             }
         }
     }
 
     static class Bridge {
-        int x, y, path;
+        int x, y;
         int IsNum;
 
-        Bridge(int x, int y, int path, int IsNum) {
+        Bridge(int x, int y, int IsNum) {
             this.x = x;
             this.y = y;
-            this.path = path;
             this.IsNum = IsNum;
         }
     }
