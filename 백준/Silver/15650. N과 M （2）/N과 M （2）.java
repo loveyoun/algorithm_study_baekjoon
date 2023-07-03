d9f206947a24437fa42f565748d8b97f79057840
lemon_silver_candy <lovekite0406@naver.com>
2023-07-03 23:12:22 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,51 +1,50 @@
+// 내 풀이
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    //public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static StringBuilder sb;
-    public static boolean[] flag;
+    public static StringBuilder sb = new StringBuilder();
+    public static boolean[] visited;
     public static int[] tmp;
     public static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
         String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]); // 1 ~ N
-        M = Integer.parseInt(s[1]); // M 열
-        flag = new boolean[N + 1];
-        tmp = new int[M + 1];
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
 
+        visited = new boolean[N];
+        tmp = new int[M];
 
-        /** 재 귀 함 수 (한 칸을 채우는데, 해당 iteration 에서 1 ~ N 중 썼는지 안 썼는지 계속 확인) 호출 **/
+        /** 재귀함수(한 칸을 채우는데, 해당 iteration 에서 (index+1) ~ N 중에서 썼는지 안 썼는지 계속 확인) **/
         recur(M);
+
         System.out.print(sb);
     }
 
-
-    static void recur(int m) { // m : 남은 자리
-        /** Base case : M 열 다 채웠을 때 **/
+    public static void recur(int m) {
         if (m == 0) {
-            for (int i = M; i >= 1; i--) sb.append(tmp[i] + " "); //bw.write("\n"); bw.flush();
+            for (int i = 0; i < M; i++) sb.append(tmp[i]).append(" ");
             sb.append("\n");
 
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (flag[i]) continue;
+        int i;
+        for (i = N; i >= 1; i--) if (visited[i - 1]) break; /** 어디까지 채웠는지 확인 -> 인자로 넘겨줘도 되겠다(dfs, depth) **/
+        for (i += 1; i <= N; i++) {
+            //if (visited[i - 1]) continue;
 
-            if (m != M && i < tmp[m + 1]) continue; /** 추가 **/ /*break;*/
-
-            flag[i] = true;  // 방명록: 퍼가요 ~
-            tmp[m] = i;      // tmp[M]부터. tmp[0] 부터 : tmp[M - m]
+            visited[i - 1] = true;
+            tmp[M - m] = i; // tmp[0] 부터
 
             recur(m - 1);
 
-            flag[i] = false; // 안 읽음으로 처리 (백트래킹)
+            visited[i - 1] = false;
         }
     }
 
