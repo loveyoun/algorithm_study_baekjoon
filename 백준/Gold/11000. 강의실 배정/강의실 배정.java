fa79d2e998b22e8450bc181b24085cbd89ae8a19
loveyoun7 <lovekite0406@naver.com>
2023-07-03 12:15:26 +0900

    [Gold V] Title: 강의실 배정, Time: 768 ms, Memory: 68184 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -10,26 +10,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<int[]> classes = new PriorityQueue<>((o1, o2) -> {
-            if (o1[0] == o2[0]) return o1[1] - o2[1];
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> {
+            //if (o1[0] == o2[0]) return o1[1] - o2[1];
             return o1[0] - o2[0];   // 시작 시간 오름차순
         });
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            classes.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            queue.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
 
+
         PriorityQueue<int[]> rooms = new PriorityQueue<>((o1, o2) -> {
-            if (o1[1] == o2[1]) return o1[0] - o2[0];
+            //if (o1[1] == o2[1]) return o1[0] - o2[0];
             return o1[1] - o2[1];   // 끝나는 시간 오름차순
         });
 
         int result = 0;
-        rooms.add(classes.poll());
-        while (!classes.isEmpty()) {
-            int[] c = classes.poll();
-            if (!rooms.isEmpty() && rooms.peek()[1] <= c[0]) rooms.poll();
-            rooms.add(c);
+        rooms.add(queue.poll());
+        while (!queue.isEmpty()) {
+            int[] q = queue.poll();
+            if (!rooms.isEmpty() && rooms.peek()[1] <= q[0]) rooms.poll();
+            rooms.add(q);
 
             result = Math.max(result, rooms.size());
         }
