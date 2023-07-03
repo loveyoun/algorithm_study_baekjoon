77ced2b9242cd3229a2d2c7012f02dfa377c3a4a
lemon_silver_candy <lovekite0406@naver.com>
2023-07-04 01:40:24 +0900

    [Gold V] Title: 신기한 소수, Time: 204 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,39 +1,44 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-  static int N;
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    N = in.nextInt();
 
-    // 일의 자리 소수는 2 3 5 7 이므로 4개 수에서만 시작
-    DFS(2, 1);
-    DFS(3, 1);
-    DFS(5, 1);
-    DFS(7, 1);
-  }
-  static void DFS(int number, int jarisu) {
-    if (jarisu == N) {
-      if (isPrime(number)) {
-        System.out.println(number);
-      }
-      return;
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        // 예시를 봐서도 2, 3, 5, 7로 시작하는 것만 봐도 된다는 걸 알 수 있다.
+        DFS(2, 1);
+        DFS(3, 1);
+        DFS(5, 1);
+        DFS(7, 1);
+
+        System.out.println(sb);
     }
-    for (int i = 1; i < 10; i++) {
-      
-      if (i % 2 == 0) { // 짝수이면 더 이상 탐색할 필요가 없음
-        continue;
-      }
-     
-      if (isPrime(number * 10 + i)) { // 소수이면 재귀로 자리수를 늘려갑니다.
-        DFS(number * 10 + i, jarisu + 1);
-      }
+
+    static void DFS(int num, int digit) {
+        if (digit == N) {
+            if (isPrime(num)) sb.append(num).append("\n");
+            return;
+        }
+
+        for (int i = 1; i < 10; i++) { // 1 ~ 9
+            if (i % 2 == 0) continue;
+
+            int next = num * 10 + i;
+
+            if (isPrime(next)) DFS(next, digit + 1);
+        }
     }
-  }
-  static boolean isPrime(int num) {
-    for (int i = 2; i <= num / 2; i++)
-      if (num % i == 0)
-        return false;
-    return true;
-  }
+
+    static boolean isPrime(int num) {
+        // classic "isPrime". Non-aristostenes
+        for (int i = 2; i <= Math.sqrt(num); i++)
+            if (num % i == 0) return false;
+
+        return true;
+    }
+
 }
