99569bb83b99dec9b9d860f1b597f247ffeb1cd2
lemon_silver_candy <lovekite0406@naver.com>
2023-07-06 16:35:50 +0900

    [Silver III] Title: N과 M (8), Time: 168 ms, Memory: 16896 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] arr, tmp;
+    public static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken()); // m 줄
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+
+        tmp = new int[m];
+        recur(0, 0);
+        System.out.println(sb);
+    }
+
+
+    static void recur(int depth, int idx) { // depth 에서 idx 부터
+        // Base case
+        if (depth == m) {
+            for (int i = 0; i < m; i++) sb.append(tmp[i]).append(" ");
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            tmp[depth] = arr[i];
+
+            recur(depth + 1, i); // i + 1 : 중복 없이
+        }
+    }
+
+}
