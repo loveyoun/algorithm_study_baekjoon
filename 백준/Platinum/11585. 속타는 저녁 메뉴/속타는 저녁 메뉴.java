7b7de7c971e8145d822140fffe984548420396f7
lemon_silver_candy <lovekite0406@naver.com>
2023-07-12 18:55:11 +0900

    [Platinum V] Title: 속타는 저녁 메뉴, Time: 912 ms, Memory: 163228 KB -BaekjoonHub

---
+++ 백준/Platinum/11585. 속타는 저녁 메뉴/속타는 저녁 메뉴.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n;
+    public static int[] table;
+    public static String text, ptr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        ptr = br.readLine().replaceAll(" ", "");
+        char[] tmp = br.readLine().replaceAll(" ", "").toCharArray();
+        char[] txt = new char[2 * n - 1];
+        for (int i = 0; i < 2 * n - 1; i++) txt[i] = tmp[i % n];
+        text = String.valueOf(txt);
+
+
+        table = new int[n];
+        setTable();
+
+
+        int result = kmp();
+        int divisor = gcd(n, result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(result / divisor);
+        sb.append('/');
+        sb.append(n / divisor);
+
+        System.out.print(sb);
+    }
+
+    public static void setTable() {
+        int j = 0;
+
+        // j: 움직(앞), i: 기준(뒤)
+        for (int i = 1; i < ptr.length(); i++) {
+            while (j > 0 && ptr.charAt(i) != ptr.charAt(j))
+                j = table[j - 1];
+
+            if (ptr.charAt(i) == ptr.charAt(j))
+                table[i] = ++j;
+        }
+
+    }
+
+    public static int kmp() {
+        int count = 0;
+        int j = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            while (j > 0 && text.charAt(i) != ptr.charAt(j))
+                j = table[j - 1];
+
+            if (text.charAt(i) == ptr.charAt(j)) {
+                if (j == ptr.length() - 1) {
+                    count++;
+                    j = table[j];
+                } else j++;
+            }
+        }
+
+        return count;
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+}
