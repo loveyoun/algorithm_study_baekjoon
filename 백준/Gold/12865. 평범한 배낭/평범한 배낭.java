6cfc1438d4e434217c404547f83030c61011f88b
lemon_silver_candy <lovekite0406@naver.com>
2023-07-21 02:20:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 548 ms, Memory: 55756 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -11,8 +11,9 @@ public class Main {
         n = read();   // 물건 개수
         k = read();   // 감당 무게
 
-        dp = new int[k + 1][n + 1];   // [weight][index]
-        its = new Item[n + 1];
+        dp = new int[k + 1][n];  // [weight][index]
+
+        its = new Item[n];
         for (int i = 0; i < n; i++) {
             int w = read();
             int v = read();
@@ -20,54 +21,102 @@ public class Main {
             its[i] = new Item(w, v);
             /*dp[w] = Math.max(dp[w], v);*/
         }
-        its[n] = new Item(0, 0);
+
         Arrays.sort(its); /***/
 
 
-        dfs(0, 0);
+        System.out.println(DP(n - 1, k));
 
-        System.out.println(result);
+        //System.out.println(result);
     }
 
 
-    /** 3) Bottom - up **/
-//    static void dfs() {
+    static int DP(int idx, int bag) {
+        // 물건 없어도 물건들 조합으로 dp는 생길 수 있다.
+
+        // Base case
+        if (idx == -1) return 0;
+
+        /* 이미 업데이트 된 경우 */
+        if (dp[bag][idx] != 0) return dp[bag][idx];
+
+
+        for (int i = idx; i >= 0; i--) {
+            int w = its[i].w;
+            int v = its[i].v;
+            if (w > bag) continue; /*|| bag - w > w*/
+
+            dp[bag][idx] = Math.max(dp[bag][idx], DP(i - 1, bag - w) + v);  /*dp[bag]*/
+        }
+
+        return dp[bag][idx];
+    }
+
+
+//    static void DP() {  //System.out.println(dp[k]);
 //        for (int i = 0; i < n; i++) {
-//            int weight = read(), value = read();
+//            int w = read(), v = read();
 //
-//            for (int j = k; j >= weight; j--)  // k ~ weight
-//                dp[j] = Math.max(dp[j], dp[j - weight] + value);
+//            for (int j = k; j >= w; j--)   // k ~ weight 배낭
+//                dp[j] = Math.max(dp[j], dp[j - w] + v);  // 해당 물건 넣었을때 vs 안 넣었을 때. `누적된다`
 //        }
 //
-//        //System.out.println(dp[k]);
 //    }
 
     /**
      * 1) Bottom - up
      **/
-    static void dfs(int idx, int weight) {  //its[n] = new Item(0, 0);
-        int w = dp[weight][idx];
-        for (int i = idx + 1; i <= n; i++) {
-            int sum = weight + its[i].w;
-            if (sum > k) break;  // continue; 정렬을 하지 않으면 뒤에 것들은 다 크지 않다
-
-            int new_value = w + its[i].v;
-
-            if (dp[sum][i] >= new_value) continue;
-
-            if (dp[sum][i - 1] >= new_value) {
-                dp[sum][i] = dp[sum][i - 1];
-                continue;
-            }
-
-
-            dp[sum][i] = new_value;
-            result = Math.max(result, dp[sum][i]);
-
-            dfs(i, sum);
-        }
-
-    }
+//    static void BT_DP(int idx, int w) {  //its[n] = new Item(0, 0);
+//        int v = dp[w][idx];
+//
+//        for (int i = idx + 1; i <= n; i++) {  // 1 ~ n 번 인덱스
+//            int bag = w + its[i].w;
+//            if (bag > k) break;
+//
+//            int new_v = v + its[i].v;
+//
+//            // 3개 비교
+//            if (dp[bag][i] >= new_v) continue;
+//            if (dp[bag][i - 1] >= new_v) {
+//                dp[bag][i] = dp[bag][i - 1];
+//                continue;
+//            }
+//
+//            dp[bag][i] = new_v;
+//            result = Math.max(result, new_v);
+//
+//            BT_DP(i, bag);
+//        }
+//
+//    }
+//    static void BT_DP(int idx, int weight) {   //its[n] = new Item(0, 0);
+//        for (int i = idx + 1; i <= n; i++) {
+//            int sum = weight + its[i].w;
+//            if (sum > k) break;
+//
+//            if (dp[sum][i] >= dp[weight][idx] + its[i].v) continue;
+//
+//            dp[sum][i] = dp[weight][idx] + its[i].v;
+//            result = Math.max(result, dp[sum][i]);
+//
+//            BT_DP(i, sum);
+//        }
+//
+//    }
+//    static void BT_DP(int idx, int weight) {  //its[0] = new Item(0, 0);
+//        for (int i = idx + 1; i <= n; i++) {
+//            int sum = weight + its[i].w;
+//            if (sum > k) continue;  // 정렬을 하지 않으면 뒤에 것들은 다 크지 않다
+//
+//            if (dp[sum][i] >= dp[weight][idx] + its[i].v) continue;
+//
+//            dp[sum][i] = dp[weight][idx] + its[i].v;
+//            result = Math.max(result, dp[sum][i]);
+//
+//            BT_DP(i, sum);
+//        }
+//
+//    }
 
 
 //    static void fail(int idx, int weight) {  //its[n] = new Item(0, 0);
@@ -87,9 +136,8 @@ public class Main {
 //        }
 //
 //    }
-
-
-//    static int fail(int idx) { /* 1 + 2 + 1 */  //its[n] = new Item(0, 0);
+//
+//    static int fail(int idx) { /* 1 + 2 + 1 */
 //        // 물건 없어도 dp는 생길 수 있다.
 //
 //        int w = its[idx].w;
@@ -100,34 +148,15 @@ public class Main {
 //        for (int i = idx + 1; i < n; i++) {
 //            int new_w = w + its[i].w;
 //            if (new_w > k) break;
-
+//
 //            dp[new_w] = Math.max(dp[new_w], dp[w] + dfs(i));
 //        }
 //
 //        return dp[w];
 //    }
-
-
+//
 //    static int fail(int w) {  //its[n] = new Item(k, 0); /* 같은 값 2개 불가능, 1 + 2 + 1 */
-//        // 물건 없어도 dp는 생길 수 있다.
-//
-//        // 이미 업데이트 된 경우
-//        if (visited[w]) return dp[w];
-//
-//        visited[w] = true;
-//        // dp 기본값 : value or -1 (물건 있을 때 없을 떄)
-//
-//        for (int i = 0; i < n; i++) {
-//            int new_w = its[i].w;
-//            if (w <= new_w) continue;
-//
-//            int plus = w - new_w;
-//            if (dfs(plus) == -1) continue;
-//
-//            dp[w] = Math.max(dp[w], dfs(new_w) + dp[plus]);
-//        }
-//
-//        return dp[w];
+
 //    }
 
 
