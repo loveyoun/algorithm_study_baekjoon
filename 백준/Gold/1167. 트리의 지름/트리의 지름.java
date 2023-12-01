d607f520261fdf07789c145f4653d09b6cfece1b
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2023-12-01 15:22:15 +0900

    [Gold II] Title: 트리의 지름, Time: 836 ms, Memory: 104568 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -5,84 +5,93 @@ import java.util.*;
 
 public class Main {
 
-    //<Integer> -> <Edge>
-    static ArrayList<Node>[] arrLst;
-    static boolean visited[];
-    static int[] distance;
+    static ArrayList<Node>[] tree;
+    static boolean[] visited;
+    //    static int[][] weight;
+    static int[] dist;
 
     public static void main(String[] args) throws IOException {
-        /** 1067_Algorithm_트리의 지름 : 트리(BFS, 큐), 스택
-         **/
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        arrLst = new ArrayList[N + 1];
+        int V = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < N + 1; i++) arrLst[i] = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
+        tree = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) tree[i] = new ArrayList<>();
+//        weight = new int[V + 1][V + 1];
+
+        for (int i = 1; i <= V; i++) {
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            while (true) {
-                int E = Integer.parseInt(st.nextToken());
-                if (E == -1) break;
-                int V = Integer.parseInt(st.nextToken());
-                arrLst[S].add(new Node(E, V));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            while (b != -1) {
+                int w = Integer.parseInt(st.nextToken());
+                tree[a].add(new Node(b, w));
+//                weight[a][b] = w;  weight[b][a] = w;
+
+                b = Integer.parseInt(st.nextToken());
             }
+
         }
 
-        /**
-         임의의 노드(1) 기준으로 가장 거리가 큰 값을 가진 노드 구하기.
-         : 임의의 노드를 루트 노드로 생각 -> 가장 거리가 큰 값을 가진 노드는 가장 꼬래비 일 것.
-         (가장 큰 노드를 가지거나, 가장 많은 엣지를 가진 노드)
-         왼쪽(오른쪽) 꼬래비 -> 오른쪽(왼쪽) 꼬래비 로 다시 거리 탐색.
-         == 루트 노드를 새롭게 생각하는 것.
-         **/
-        distance = new int[N + 1];
-        visited = new boolean[N + 1];
-        BFS(1);
+
+        dist = new int[V + 1];
+        visited = new boolean[V + 1];
+        dfs(1, 0);
+//        bfs(1);
         int max_idx = 1;
-        for (int i = 2; i < N + 1; i++) max_idx = distance[max_idx] > distance[i] ? max_idx : i;
+        for (int i = 2; i <= V; i++) max_idx = dist[max_idx] > dist[i] ? max_idx : i;
 
-        distance = new int[N + 1];
-        visited = new boolean[N + 1];
-        BFS(max_idx);
+        dist = new int[V + 1];
+        visited = new boolean[V + 1];
+        dfs(max_idx, 0);
+//        bfs(max_idx);
 
-        Arrays.sort(distance);
-
-        System.out.println(distance[N]);
+        Arrays.sort(dist);
+        System.out.println(dist[V]);
     }
 
-    static void BFS(int index) {
-        Queue<Integer> queue = new LinkedList<>();
+    static void dfs(int s, int d) {
+        visited[s] = true;
 
-        visited[index] = true;
-        queue.add(index);
-
-        /** 여기서 계속 돌아감. **/
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-
-            // 단방향으로 하면 안되나?
-            for (Node node : arrLst[now]) {
-                int k = node.kid;
-                int v = node.value;
-                if (!visited[k]) {
-                    visited[k] = true;
-                    queue.add(k);
-                    distance[k] = distance[now] + v;
-                }
-            }
+        for (Node next : tree[s]) {
+            if (!visited[next.e]) dfs(next.e, d + next.w);
         }
+        if (tree[s].size() == 1) dist[s] = d;
+
+        visited[s] = false;
     }
+
+    static void bfs(int s) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        visited[s] = true;
+
+        while (!q.isEmpty()) {
+            int v = q.poll();
+
+            for (Node next : tree[v]) {
+                if (visited[next.e]) continue;
+
+                dist[next.e] = dist[v] + next.w;   //weight[next][v];
+                q.add(next.e);
+                visited[next.e] = true;
+            }
+
+        }
+
+    }
+
 
     static class Node {
-        int kid, value;
 
-        public Node(int kid, int value) {
-            this.kid = kid;
-            this.value = value;
+        int e, w;
+
+        Node(int e, int w) {
+            this.e = e;
+            this.w = w;
         }
+
     }
+
 
 }
