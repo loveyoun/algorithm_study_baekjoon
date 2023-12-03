9ae9b22e0519e2d6342887bd0e6e5f2f6ad05762
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2023-12-03 10:22:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 200 ms, Memory: 15756 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,50 +1,37 @@
-/**
- * Top - down
- **/
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
-    static int[][] dp;
-    static int[] w, v;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        w = new int[n];
-        v = new int[n];
-        dp = new int[n][k + 1];  // [n개 물건][가방 무게 합]
+        int[] dp = new int[k + 1];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            for (int j = k; j >= w; j--) { // j = 5, w = 2, j - w = 3
+                if (dp[j] >= dp[j - w] + v) continue;
+                
+                if (dp[j - w] > 0 || (j - w) == 0)
+                    dp[j] = dp[j - w] + v;
+            }
+
         }
 
 
-        System.out.println(knapsack(n - 1, k));
+        int max = 0;
+        for (int i = 1; i <= k; i++) max = Math.max(max, dp[i]);
+
+        System.out.print(max);
     }
 
-    static int knapsack(int i, int k) {
-        // Base case
-        if (i < 0) return 0;
-
-        // 업데이트 되었다면
-        if (dp[i][k] != 0) return dp[i][k];  /****/
-
-        // 현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색)
-        if (w[i] > k) dp[i][k] = knapsack(i - 1, k);
-        else dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - w[i]) + v[i]);
-
-
-        return dp[i][k];
-    }
 
 }
