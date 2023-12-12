fa03629ec44a7b8ca8b0b09c41b978d80dedf5ed
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2023-12-12 14:27:14 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,27 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-        
+
         int answer = 0;
-        answer += mySum(st.nextToken());
-        while(st.hasMoreElements()){
-            int temp = mySum(st.nextToken());
-            answer -= temp;
+        /* anwer += Integer.parseInt(st.nextToken());
+         * 오류 이유 : 처음이 10+34 - 10+20+30 ...이럴 수 있어서.
+         */
+        answer += sumPlus(st.nextToken());
+        while (st.hasMoreElements()) {
+            int tmpSum = sumPlus(st.nextToken());
+
+            answer -= tmpSum;
         }
-        
+
+
         System.out.println(answer);
     }
-    
-    
-    static int mySum(String a){
+
+
+    static int sumPlus(String str) {
         int sum = 0;
-        String temp[] = a.split("\\+");
-        for(int i=0;i<temp.length;i++) sum += Integer.parseInt(temp[i]);
-        
+
+        String[] arr = str.split("\\+");
+        for (String s : arr) sum += Integer.parseInt(s);
+
         return sum;
     }
+
+
 }
