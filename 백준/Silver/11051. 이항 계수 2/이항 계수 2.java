2f0ac146ba18721e42dc55e2a40b1103c7e3528a
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2023-12-14 10:43:31 +0900

    [Silver II] Title: 이항 계수 2, Time: 140 ms, Memory: 18084 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    final static int MOD = 10007;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+
+        // 1. 구현
+//        int answer = 1;
+//        for (int i = N; i > N - K; i--) // 5Ｃ2, 5Ｃ5
+//            answer *= i;
+//        for (int i = 2; i <= K; i++) answer /= i;
+//
+//        System.out.println(answer);
+
+
+        // 2. 조합
+        int[][] dp = new int[N + 1][N + 1];
+
+        // 5Ｃ0=1, 5Ｃ1=5, 5Ｃ5=1
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 1;
+            dp[i][1] = i;
+            dp[i][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; j <= i - 1; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % MOD;
+            }
+        }
+
+        System.out.println(dp[N][K]);
+
+
+        // 3. DP
+
+    }
+
+    static int factorial(int n) {
+        if (n <= 1) return 1;
+
+        int ret = 1;
+        for (int i = n; i >= 2; i--)
+            ret *= i;
+
+        return ret;
+    }
+
+
+}
