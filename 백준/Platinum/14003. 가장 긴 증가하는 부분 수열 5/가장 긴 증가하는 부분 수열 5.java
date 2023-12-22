70461df075fb676a7b4207f2c43575a9125d5bba
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2023-12-22 09:51:40 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 768 ms, Memory: 129524 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -5,67 +5,75 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static final int MAX_LENGTH = 1000001;
-    static int N, maxLength;
+    static final int MAX_LENGTH = 1_000_000 + 1;
+    static int N;
     static int[] arr = new int[MAX_LENGTH];
-    static int[] fake_LIS = new int[MAX_LENGTH];
-    static int[] max_len = new int[MAX_LENGTH];
-    static int[] answer = new int[MAX_LENGTH];
+    static int[] fakeLIS = new int[MAX_LENGTH];
+    static int[] LISLen = new int[MAX_LENGTH];
+    static int[] realLIS = new int[MAX_LENGTH];
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
 
-        fake_LIS[++maxLength] = arr[1];
-        max_len[1] = 1;
-
+        int size = 1;
+        fakeLIS[size] = arr[1];
+        LISLen[1] = 1;
         for (int i = 2; i <= N; i++) {
-            if (fake_LIS[maxLength] < arr[i]) {
-                fake_LIS[++maxLength] = arr[i];
-                max_len[i] = maxLength;
+            if (fakeLIS[size] < arr[i]) { // 10 + 20
+                fakeLIS[++size] = arr[i];
+                LISLen[i] = size;
             } else {
-                int index = binarySearch(1, maxLength, arr[i]);
+                int idx = binarySearch(1, size, arr[i]);
 
-                // 즉시 치.환.
-                fake_LIS[index] = arr[i];
-
-                // index 는 길이이자 내 값이 들어간 인덱스 
-                // -> 그래서 0 이 아니라 1 부터 시작했군. 0 이었어도 maxLength만 +1 해주면 됨.
-                max_len[i] = index;
+                fakeLIS[idx] = arr[i]; // 10 -> 1
+                LISLen[i] = idx;
             }
+
         }
-        System.out.println(maxLength);
+        System.out.println(size);
 
 
-        int tmp = maxLength;
-        int the_before = fake_LIS[maxLength] + 1;
+        int idx = size;
+        int pre = fakeLIS[size] + 1;
         for (int i = N; i >= 1; i--) {
-            if (max_len[i] == tmp && arr[i] < the_before) {
-                answer[tmp] = arr[i];
+            if (LISLen[i] == idx && arr[i] < pre) {
+                realLIS[idx] = arr[i];
 
-                the_before = arr[i];
-                tmp--;
+                pre = arr[i];
+                idx--;
             }
+
         }
 
-        for (int i = 1; i <= maxLength; i++)
-            System.out.print(answer[i] + " ");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= size; i++)
+            sb.append(realLIS[i]).append(" ");
+
+        System.out.println(sb);
     }
 
-    // 탐색해야겠다... 정렬되어있네? -> 즉.시. 이분 탐색
-    static int binarySearch(int left, int right, int now) {
-        while (left < right) {
-            int mid = (left + right) / 2;
 
-            if (fake_LIS[mid] < now) left = mid + 1;
-            else right = mid;
+    static private int binarySearch(int s, int e, int num) {
+        while (s < e) {
+            int mid = (s + e) / 2;
+
+            if (fakeLIS[mid] < num)
+                s = mid + 1;
+            else e = mid;  // >= num
         }
 
-        // 처음으로 now <= fake_LIS[left]
-        return left;
+        // x x x o o o
+        // s e -> s(e)
+        // 처음으로 num <= [s]
+        return s;
     }
+
 
 }
