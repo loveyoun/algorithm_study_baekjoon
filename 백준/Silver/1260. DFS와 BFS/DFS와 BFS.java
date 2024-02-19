c377ea1e9138aa368ee092300453f74f0e29f33d
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2024-02-19 17:23:04 +0900

    [Silver II] Title: DFS와 BFS, Time: 216 ms, Memory: 20544 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static ArrayList<Integer>[] graph;
+    static Node[] graph;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
@@ -16,20 +18,40 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+        graph = new Node[N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            graph[a].add(b);
-            graph[b].add(a);
+            Node now = graph[a];
+            if (now == null || b < now.v) graph[a] = new Node(b, graph[a]);
+            else { // '2' -> 4 -> '5' -> null: 3, 6 삽입
+                while (b > now.v) {
+                    if (now.next == null || now.next.v > b) break;
+                    now = now.next;
+                }
+                now.next = new Node(b, now.next);
+            }
+
+            now = graph[b];
+            if (now == null || a < now.v) graph[b] = new Node(a, graph[b]);
+            else {
+                while (a > now.v) {
+                    if (now.next == null || now.next.v > a) break;
+                    now = now.next;
+                }
+                now.next = new Node(a, now.next);
+            }
         }
-
-        for (int i = 1; i <= N; i++) Collections.sort(graph[i]);
-//        for (int i = 1; i <= N; i++) graph[i].sort((n1, n2) -> n1 - n2);
-
+//        for (int i = 1; i <= N; i++) {
+//            Node now = graph[i];
+//            while (now != null) {
+//                System.out.print(now.v + " ");
+//                now = now.next;
+//            }
+//            System.out.println();
+//        }
 
         visited = new boolean[N + 1];
         dfs(V);
@@ -45,12 +67,11 @@ public class Main {
         visited[v] = true;
         sb.append(v).append(" ");
 
-        for (int new_ : graph[v]) {
-            if (!visited[new_]) {
-                dfs(new_);
+        for (Node node = graph[v]; node != null; node = node.next) {
+            if (!visited[node.v]) {
+                dfs(node.v);
             }
         }
-
     }
 
     static void bfs(int s) {
@@ -62,16 +83,24 @@ public class Main {
             int v = q.poll();
             sb.append(v).append(" ");
 
-            for (int new_ : graph[v]) {
-                if (visited[new_]) continue;
+            for (Node node = graph[v]; node != null; node = node.next) {
+                int next = node.v;
+                if (visited[next]) continue;
 
-                q.add(new_);
-                visited[new_] = true;
+                q.add(next);
+                visited[next] = true;
             }
-
         }
-
     }
 
+    static class Node {
+        int v;
+        Node next;
+
+        Node(int v, Node next) {
+            this.v = v;
+            this.next = next;
+        }
+    }
 
 }
