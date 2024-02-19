3d962aba9f0ead3b54b2445ef3037f3b8547120f
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2024-02-19 15:57:03 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 704 ms, Memory: 141844 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,49 +1,59 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main{
-    
-    static ArrayList<Integer>[] arrLst;
-    static boolean visited[];
-    
-    public static void main(String[] args) throws IOException{
+public class Main {
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        String[] tmp = br.readLine().split(" ");
-        int N = Integer.parseInt(tmp[0]);
-        int E = Integer.parseInt(tmp[1]);
-        
-        arrLst = new ArrayList[N+1]; //이거 없으면 NullPointErorr
-        visited = new boolean[N+1];
-        for(int i=1;i<N+1;i++) arrLst[i] = new ArrayList<Integer>();
-        for(int i=0;i<E;i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        //String[] tmp = br.readLine().split(" ");
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i < N + 1; i++)
+            graph[i] = new ArrayList<>(); /***/
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());            
-            arrLst[s].add(e); arrLst[e].add(s);
+            int e = Integer.parseInt(st.nextToken());
+
+            graph[s].add(e);
+            graph[e].add(s);
         }
-        
-        int count = 0;
-        for(int i=1;i<N+1;i++){    //탐색 쉽게 배열 인덱스 맞춰주기
-            if(!visited[i]){
-                count++; //연결요소들 다 DFS되니까 visited == true;
-                DFS(i);    
-            }
+
+
+        visited = new boolean[N + 1]; /***/
+        int cc = 0;
+        for (int i = 1; i < N + 1; i++) {
+            if (visited[i]) continue;
+            cc++;
+            dfs(i);
         }
-        
-        System.out.println(count);
+
+        System.out.println(cc);
     }
-    
-    public static void DFS(int s){
-        if(visited[s]) return; //지나간 곳 가지 않는다.
-        visited[s] = true; //지나갔다고 표시한다.
-        for(int i : arrLst[s]){ //for(int i=0;i<A[s].length;i++)
-            if(!visited[i]) DFS(i);
-        }   
+
+    static void dfs(int v) {
+//        if (visited[v]) return;
+        visited[v] = true; /***/
+
+        for (int i = 0; i < graph[v].size(); i++) {
+            //for (int next : graph[v])
+            int next = graph[v].get(i);
+
+            if (visited[next]) continue;
+            dfs(next);
+        }
+
     }
-    
+
+
 }
