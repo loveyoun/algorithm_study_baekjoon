b0def3ecdb3b07ba500563f966145b63163a8c4b
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2024-02-20 09:33:23 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,56 +1,79 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main{
-    static int[][] A;
+public class Main {
+
+    static boolean[][] graph, visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     static int N, M;
-    static boolean[][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        A = new int[N][M];
-        visited = new boolean[N][M];
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
-            for(int j=0;j<M;j++) A[i][j] = Integer.parseInt(line.substring(j, j+1));           
-        }
-        
-        BFS(0,0);
-        
-        System.out.println(A[N-1][M-1]);
-    }
-    
-   
-    static void BFS(int i, int j){
-        //초기 설정
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {i,j});
-        visited[i][j] = true;
-        
-        while(!q.isEmpty()){
-            int now[] = q.poll();
-            for(int k=0;k<4;k++){
-                //좌표 움직이고
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                //좌표 유효성 검사하고
-                if(x >= 0 && y >= 0 && x < N && y < M){
-                    //좌표 상태 확인(갈 수 있는 곳인지, 이미 간 곳은 아닌지)
-                    if(A[x][y] != 0 && !visited[x][y]){
-                        visited[x][y] = true;
-                        //깊이 업데이트
-                        A[x][y] = A[now[0]][now[1]] + 1;
-                        //큐에 넣어주기 rear add->2 4 0->poll front 
-                        q.add(new int[] {x,y});
-                    }
-                }
+
+        graph = new boolean[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            String row = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                if (row.charAt(j - 1) == '1') graph[i][j] = true;
+                else graph[i][j] = false;
             }
+
         }
+
+
+        visited = new boolean[N + 1][M + 1];
+        System.out.println(bfs(1, 1));
     }
+
+    static int bfs(int x, int y) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y, 1));
+        visited[x][y] = true;
+
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int nx = p.x + dx[k];
+                int ny = p.y + dy[k];
+                int nl = p.len + 1;
+                if (nx == N && ny == M) {
+                    cnt = nl;
+                    break;
+                }
+
+                if (nx < 1 || nx > N || ny < 1 || ny > M || visited[nx][ny] || !graph[nx][ny]) continue;
+                q.add(new Point(nx, ny, nl));
+                visited[nx][ny] = true;
+            }
+
+            if (cnt > 0) break;
+        }
+
+        return cnt;
+    }
+
+
+    static class Point {
+
+        int x, y, len;
+
+        Point(int x, int y, int len) {
+            this.x = x;
+            this.y = y;
+            this.len = len;
+        }
+
+    }
+
+
 }
