90580633a8744ab273e6760f38299b5710a4f99a
jxxxyxxx <52818342+loveyoun@users.noreply.github.com>
2024-02-28 13:53:51 +0900

    [Gold V] Title: 집합의 표현, Time: 652 ms, Memory: 56624 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,54 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+
     static int[] parent;
-    
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        parent = new int[N+1];
-        for(int i=0;i<N+1;i++) parent[i] = i;
-        
-        for(int i=0;i<M;i++){
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) parent[i] = i;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int question = Integer.parseInt(st.nextToken());
+            int q = Integer.parseInt(st.nextToken());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-            
-            if(question == 0) union(n1, n2);
-            else{
-                if(isSib(n1, n2)) System.out.println("YES");
-                else System.out.println("NO");
+
+            if (q == 0) union(n1, n2);
+            else {
+                if (isSame(n1, n2))
+                    sb.append("YES").append("\n");
+                else sb.append("NO").append("\n");
             }
         }
+
+        System.out.print(sb);
+//        for (int e : parent) System.out.print(e + " ");
     }
-    
-    
-    static int find(int index){
-        // 1 3 5 7
-        // 1 1 3 5
-        if(index != parent[index]){
-            //index = parent[index]. parent바꿀 수 없음.
-            //내 부모들을 업데이트 해주고 싶음.
-            return parent[index] = find(parent[index]);
-            //parent[7] = find(5) = find(parent[5]==3) ... = 1
-        }else return index;
+
+    static void union(int a, int b) {
+        int ca = a; //pa = find(a);
+        int cb = b; //pa = find(b);
+        while (a != parent[a]) a = find(a);
+        while (b != parent[b]) b = find(b);
+        if (ca != cb) parent[b] = a;
+        parent[cb] = a;
     }
-    
-    static void union(int a, int b){
-        //parent[find(b)] = parent[a];
-        //parent를 깊숙히 탐색해야함.
-        //중간에 바뀐 parent 고려.
-        a = find(a); b = find(b);
-        if(a != b) parent[b] = a; //b가 대표노드였으면, 자녀노드들은 각자 find하면 그 위에가 바뀜.
+
+    static boolean isSame(int a, int b) {
+        while (a != parent[a]) a = find(a);
+        while (b != parent[b]) b = find(b);
+
+        return a == b; //find(a) == find(b);
     }
-    
-    static boolean isSib(int a, int b){
-        a = find(a); b = find(b);
-        if(a == b) return true;
-        else return false;
+
+    static int find(int i) {
+        return parent[i];
     }
+
 }
